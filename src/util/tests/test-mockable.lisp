2372e102c5ef391ed63aa096f678e2ad1df36afe
Arnold Noronha <arnold@tdrhq.com>
2021-04-26 16:34:34 -0700

    Add License to util files
    Differential Revision: https://phabricator.tdrhq.com/D3623
    
    GitOrigin-RevId: 54f74b7a9166ba4940d42eee7dba963ac46fb17f

--- src/util/tests/test-mockable.lisp
+++ src/util/tests/test-mockable.lisp
@@ -1,3 +1,9 @@
+;; Copyright 2018-Present Modern Interpreters Inc.
+;;
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 (defpackage :test-mockable
   (:use :cl
         :mockable
