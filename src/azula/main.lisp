b5d9bbadebc77c3776934d4d9c87b43a9dd9898d
Arnold Noronha <arnold@tdrhq.com>
2021-05-20 10:43:19 -0700

    Auto (in-package)
    Differential Revision: https://phabricator.tdrhq.com/D3664
    
    GitOrigin-RevId: 20c73eb6d8b161b6e47a09562920a82afd58b173

--- src/azula/main.lisp
+++ src/azula/main.lisp
@@ -4,13 +4,12 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(defpackage :azula/main
+(pkg:define-package :azula/main
   (:use :cl)
   (:export :config
    :azula-root
    :cache-key
            :build))
-(in-package :azula/main)
 
 (defparameter *cache-version* 1)
 
