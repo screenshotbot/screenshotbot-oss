45f3258392a49ec3758bad5ba35565aebc106fa5
Arnold Noronha <arnold@tdrhq.com>
2021-11-17 09:35:31 -0800

    Try to match the prefix of the package name as much as possible
    GitOrigin-RevId: 1df91e26be2943ac1ae13dc165ab89866e725e0a

--- src/util/emacs.lisp
+++ src/util/emacs.lisp
@@ -5,11 +5,26 @@
    #:list-packages-for-symbol))
 (in-package :util/emacs)
 
-(defun list-packages-for-symbol (name)
-  (mapcar
-   #'package-name
+(defun list-packages-for-symbol (name current-package)
+  (let ((current-package (if (str:starts-with-p ":" current-package)
+                             (str:substring 1 nil current-package)
+                             current-package)))
+    (log:info "Got package : ~s" current-package)
+   (mapcar
+    #'package-name
+    (sort
      (loop for package in (list-all-packages)
            if (a:when-let (sym (find-symbol (string-upcase name)
                                             package))
                 (eql package (symbol-package sym)))
-             collect package)))
+             collect package)
+     #'> :key (lambda (package)
+                (let ((package-name (package-name package)))
+                  (loop for i from 0 below (length current-package)
+                        while (and (< i (length package-name))
+                                   (char-equal (elt current-package i)
+                                               (elt package-name i)))
+                        finally
+                           (progn
+                             (log:info "For ~s, got ~d" package i)
+                             (return i)))))))))
