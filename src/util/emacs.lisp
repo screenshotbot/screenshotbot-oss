c64b73f8f800230a155bbbe8d902e7ee3de3bd54
Arnold Noronha <arnold@tdrhq.com>
2021-11-26 21:21:31 -0500

    Override markup/markup to markup in auto-imports
    Differential Revision: https://phabricator.tdrhq.com/D4500
    
    GitOrigin-RevId: a540f1054a355c181ba2567ada3a377c5d47027d

--- src/util/emacs.lisp
+++ src/util/emacs.lisp
@@ -5,27 +5,39 @@
    #:list-packages-for-symbol))
 (in-package :util/emacs)
 
+(defparameter *overrides*
+  `((:markup/markup . :markup)))
+
+(defun override-if-needed (package-name )
+  (loop for (from . to) in *overrides*
+        if (string= from package-name)
+          do (return (string to))
+        finally
+        (return package-name)))
+
 (defun list-packages-for-symbol (name current-package)
   (let ((current-package (if (str:starts-with-p ":" current-package)
                              (str:substring 1 nil current-package)
                              current-package)))
     (log:info "Got package : ~s" current-package)
-   (mapcar
-    #'package-name
-    (sort
-     (loop for package in (list-all-packages)
-           if (unless (string-equal current-package (package-name package))
-               (a:when-let (sym (find-symbol (string-upcase name)
-                                             package))
-                 (eql package (symbol-package sym))))
-             collect package)
-     #'> :key (lambda (package)
-                (let ((package-name (package-name package)))
-                  (loop for i from 0 below (length current-package)
-                        while (and (< i (length package-name))
-                                   (char-equal (elt current-package i)
-                                               (elt package-name i)))
-                        finally
-                           (progn
-                             (log:info "For ~s, got ~d" package i)
-                             (return i)))))))))
+    (mapcar
+     #'override-if-needed
+     (mapcar
+      #'package-name
+      (sort
+       (loop for package in (list-all-packages)
+             if (unless (string-equal current-package (package-name package))
+                  (a:when-let (sym (find-symbol (string-upcase name)
+                                                package))
+                    (eql package (symbol-package sym))))
+               collect package)
+       #'> :key (lambda (package)
+                  (let ((package-name (package-name package)))
+                    (loop for i from 0 below (length current-package)
+                          while (and (< i (length package-name))
+                                     (char-equal (elt current-package i)
+                                                 (elt package-name i)))
+                          finally
+                             (progn
+                               (log:info "For ~s, got ~d" package i)
+                               (return i))))))))))
