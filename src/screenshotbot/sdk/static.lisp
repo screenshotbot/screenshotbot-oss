1f65808c5ced600719c68722186bb94de9d00da2
Arnold Noronha <arnold@tdrhq.com>
2022-02-10 20:19:52 -0500

    Careful uploading of multiple blobs
    Differential Revision: https://phabricator.tdrhq.com/D4831
    
    GitOrigin-RevId: c02fbc7a4d307cc9155e84ee85497c9d5dcf3c4b

--- src/screenshotbot/sdk/static.lisp
+++ src/screenshotbot/sdk/static.lisp
@@ -10,6 +10,7 @@
   (ironclad:byte-array-to-hex-string (md5:md5sum-file file)))
 
 (defun upload-blob (file)
+  (log:info "Uploading file: ~a" file)
   (with-open-file (stream file :direction  :input
                                :element-type '(unsigned-byte 8))
     (let ((uri (quri:make-uri
@@ -22,8 +23,39 @@
      (sdk:put-file uri
                    stream))))
 
+
+(defun blob-check (files)
+  (let ((input (loop for file in files
+                     collect `((:file . ,(namestring file))
+                               (:hash . ,(md5-file file))
+                               (:type . ,(pathname-type file))))))
+    (sdk:request "/api/blob/check"
+                 :method :post
+                 :parameters `(("query" . ,(json:encode-json-to-string input))))))
+
+(defun upload-multiple-files (files)
+  (restart-case
+      (let ((results (blob-check files)))
+        (loop for result in results
+              for existsp = (a:assoc-value result :exists)
+              for file = (a:assoc-value result :file)
+              if (not existsp)
+                do
+                   (upload-blob file)))
+    (retry-upload-multiple-files ()
+      (upload-multiple-files files))))
+
 #+nil
 (let ((flags:*hostname* "https://staging.screenshotbot.io")
       (flags:*api-key* *key*)
       (flags:*api-secret* *secret*))
- (upload-blob "~/builds/web/update-ip.lisp"))
+  (upload-blob "~/builds/web/update-ip.lisp"))
+
+#+nil
+(let ((flags:*hostname* "https://staging.screenshotbot.io")
+      (flags:*api-key* *key*)
+      (flags:*api-secret* *secret*))
+  (upload-multiple-files
+   (list
+    "~/builds/web/update-ip.lisp"
+    "~/builds/web/Makefile")))
