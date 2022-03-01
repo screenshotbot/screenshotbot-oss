a5bd6423a6e8ed5a69a65000ea547d3454c390b1
Arnold Noronha <arnold@tdrhq.com>
2022-03-01 12:32:51 -0500

    Use a correct timeout
    Summary:
    ..
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D4886
    
    GitOrigin-RevId: 8e5f2d7e001123ef406cb174808825cbe4593ff2

---
+++ src/screenshotbot/static/assets/js/browser-tests.js
@@ -0,0 +1,49 @@
+
+function runBrowserTests() {
+    function log(msg) {
+        var $span = $("<div></div>");
+        $span.append(msg);
+        $("#results").append($span);
+    }
+
+    log("Starting up");
+
+    var counter = 1;
+    const timeout = setInterval(function () {
+        counter ++;
+        log("Timeout log for " + (counter * 100) + "ms mark");
+    }, 100);
+
+    var startTime = new Date();
+
+    function getElapsedTime() {
+        return ( (new Date()).getTime() - startTime.getTime())/1000;
+    }
+
+    function logElapsedTime() {
+        log("Took " + getElapsedTime() + " seconds");
+        clearInterval(timeout);
+    }
+
+    var domain = $("body").data("domain");
+    log("Fetching " + domain);
+
+    $.ajax({
+        url: domain,
+        method: "GET",
+        success: function (){
+            log("ERROR: We were able to fetch " + domain);
+            logElapsedTime();
+        },
+        error: function (){
+            log("SUCCESS: " + domain + " was inaccessible");
+            logElapsedTime();
+            if (getElapsedTime() > 5) {
+                log("ERROR: Took a while to result in an failed ajax request");
+            }
+        }
+
+    });
+}
+
+runBrowserTests();
