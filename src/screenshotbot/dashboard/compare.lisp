4e02b556d1dc972d31aa55d910d6850ca8b4749a
Arnold Noronha <arnold@tdrhq.com>
2022-03-21 16:54:11 -0400

    Use btn-close
    Summary: Fixes T263
    
    Maniphest Tasks: T263
    
    Differential Revision: https://phabricator.tdrhq.com/D5043
    
    GitOrigin-RevId: 9610d4858117743f8ff17590ee931bd63dd6f7cd

--- src/screenshotbot/dashboard/compare.lisp
+++ src/screenshotbot/dashboard/compare.lisp
@@ -586,11 +586,7 @@ If the images are identical, we return t, else we return NIL."
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id=modal-label >Image Comparison</h5>
-              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">
-                  &times
-                </span>
-              </button>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
             </div>
             <div class="modal-body">
               <progress-img
