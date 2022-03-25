59a69246e1b1133a7b3383b083f5d0a2a02ac0e7
Arnold Noronha <arnold@tdrhq.com>
2022-03-25 11:13:15 -0400

    Add an alert for when you're signing up with an invitation code
    Summary: Fixes t283
    
    Maniphest Tasks: T283
    
    Differential Revision: https://phabricator.tdrhq.com/D5073
    
    GitOrigin-RevId: de0ff073b95390c454cbe7ebb59cc902a75046f3

--- src/screenshotbot/login/signup.lisp
+++ src/screenshotbot/login/signup.lisp
@@ -101,10 +101,18 @@
 
 (deftag signup-get (&key invite-code plan (redirect "/"))
   (let ((login (nibble ()
-                 (signin-get :redirect redirect))))
+                 (signin-get :redirect redirect)))
+        (invite (when invite-code
+                  (invite-with-code invite-code))))
     <auth-template>
       <div class="account-pages mt-5 mb-5">
         <div class="container">
+
+          ,(when invite
+             <div class= "alert alert-info">
+               <h4 class= "mb-3 mt-0 pt-0" >Your invitation from ,(user-full-name (inviter invite)) is pending.</h4>
+               <p class= "mb-0 pb-0" >Once you create your account you will have an option of accepting the invite.</p>
+             </div>)
           <div class="row justify-content-center">
             <div class="col-lg-5">
               <div class="card">
