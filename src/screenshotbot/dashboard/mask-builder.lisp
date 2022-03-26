ed03cb6aa59dd873e8ec2fe7d5ddeee4ef03f7bd
Arnold Noronha <arnold@tdrhq.com>
2022-03-26 09:26:53 -0400

    Unnecessary use of opticl
    Summary: Fixes t291
    
    Maniphest Tasks: T291
    
    Differential Revision: https://phabricator.tdrhq.com/D5082
    
    GitOrigin-RevId: 8bee9eb461a62abd7b66e3c2278328b22fff0882

--- src/screenshotbot/dashboard/mask-builder.lisp
+++ src/screenshotbot/dashboard/mask-builder.lisp
@@ -16,7 +16,11 @@
   (:import-from #:screenshotbot/server
                 #:with-login)
   (:import-from #:screenshotbot/dashboard/run-page
-                #:mask-editor))
+                #:mask-editor)
+  (:import-from #:screenshotbot/model/image
+                #:dimension-height
+                #:dimension-width
+                #:image-dimensions))
 (in-package :screenshotbot/dashboard/mask-builder)
 
 
@@ -24,46 +28,43 @@
 
 (defun mask-editor (channel screenshot &key (redirect "/"))
   (with-login ()
-   (with-open-stream (s (open-image-stream (screenshot-image screenshot)))
-     (let* ((im (opticl:read-png-stream s))
-            (mask (assoc-value (masks channel) (screenshot-name screenshot) :test 'equal))
-            (save (nibble (json :method :post)
-                    (let ((new-mask (loop for x in (json:decode-json-from-string json)
-                                          collect
-                                          (apply 'make-instance
-                                                  'mask-rect
-                                                  :allow-other-keys t
-                                                  (alist-plist x)))))
-                      (set-channel-screenshot-mask
-                       channel
-                       (screenshot-name screenshot)
-                       new-mask))
-                    (hex:safe-redirect redirect))))
-       (destructuring-bind (h w z) (array-dimensions im)
-         (declare (ignore z))
-         <app-template>
-         <form action=save method= "POST" id= "mask-editor-form" >
+   (let* ((dim (image-dimensions screenshot))
+          (mask (assoc-value (masks channel) (screenshot-name screenshot) :test 'equal))
+          (save (nibble (json :method :post)
+                  (let ((new-mask (loop for x in (json:decode-json-from-string json)
+                                        collect
+                                        (apply 'make-instance
+                                                'mask-rect
+                                                 :allow-other-keys t
+                                                 (alist-plist x)))))
+                    (set-channel-screenshot-mask
+                     channel
+                     (screenshot-name screenshot)
+                     new-mask))
+                  (hex:safe-redirect redirect))))
+     <app-template>
+       <form action=save method= "POST" id= "mask-editor-form" >
          <h1>Edit Masks</h1>
 
          <p>
-         Masks specifies areas of the image that won't count towards screenshot comparison. For instance, you may use this to mask out animations and timestamps. Draw rectangles in the image below to specify the masked area.
+           Masks specifies areas of the image that won't count towards screenshot comparison. For instance, you may use this to mask out animations and timestamps. Draw rectangles in the image below to specify the masked area.
          </p>
 
          <p>
-         Any modifications to the mask will only affect future runs. Existing runs and reports will not be affected.
+           Any modifications to the mask will only affect future runs. Existing runs and reports will not be affected.
          </p>
 
-             <canvas id= "mask-editor" width= w height= h
-                     data-rects= (json:encode-json-to-string (coerce mask 'vector )) />
-             <img id= "mask-editor-image"
-                  src=(image-public-url (screenshot-image screenshot))
-                  style= "display:none" />
-             <script src= "/assets/js/fabric.min.js" />
+         <canvas id= "mask-editor" width= (dimension-width dim) height= (dimension-height dim)
+                 data-rects= (json:encode-json-to-string (coerce mask 'vector )) />
+         <img id= "mask-editor-image"
+              src=(image-public-url (screenshot-image screenshot))
+              style= "display:none" />
+         <script src= "/assets/js/fabric.min.js" />
 
-             <div class= "mt-3" >
-               <input type= "hidden" name= "json" value= "" />
-               <input type= "submit" id= "save-masks" class= "btn btn-primary" value = "Save Masks" />
-               <a href= "#" id= "clear-masks" class= "btn btn-danger">Clear Masks</a>
-             </div>
-           </form>
-         </app-template>)))))
+         <div class= "mt-3" >
+           <input type= "hidden" name= "json" value= "" />
+           <input type= "submit" id= "save-masks" class= "btn btn-primary" value = "Save Masks" />
+           <a href= "#" id= "clear-masks" class= "btn btn-danger">Clear Masks</a>
+         </div>
+       </form>
+     </app-template>)))
