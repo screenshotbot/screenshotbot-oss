520f979379005d12a5b5343fcdcdf50034240e2e
Arnold Noronha <arnold@tdrhq.com>
2022-04-06 23:12:27 -0400

    Calculate storage size of objects
    Differential Revision: https://phabricator.tdrhq.com/D5172
    
    GitOrigin-RevId: 2d92ad80cb9a91f3b07f287bcd93a3f608c9bc84

--- src/util/memory.lisp
+++ src/util/memory.lisp
@@ -5,17 +5,20 @@
 
 
 (defun histogram ()
-  (let ((map (make-hash-table :test #'eql)))
+  (let ((map (make-hash-table :test #'eql))
+        (sizes (make-hash-table :test #'eql)))
     (hcl:sweep-all-objects
      (lambda (obj)
-       (incf (gethash (type-of obj) map 0))))
+       (incf (gethash (type-of obj) map 0))
+       (incf (gethash (type-of obj) sizes 0)
+             (hcl:find-object-size obj))))
     (let ((list (loop for k being the hash-keys in map
                         using (hash-value v)
                       collect (cons k v))))
       (let ((ret (sort list #'> :key #'cdr)))
         (loop for (k . v) in ret
               for i below 100
-              do (format t "~a ~s~%" v k))))))
+              do (format t "~a ~s ~a~%" v k (gethash k sizes)))))))
 
 
 ;; (histogram)
