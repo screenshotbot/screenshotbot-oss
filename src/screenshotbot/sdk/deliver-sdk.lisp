561dc115feca346b874e845eb14fce5aa563dc3d
Arnold Noronha <arnold@tdrhq.com>
2022-05-25 11:28:23 -0400

    Universal binary, but we need to figure out libyaml
    GitOrigin-RevId: 657d15cce52909b203850d00c99b33182699b4b3

--- src/screenshotbot/sdk/deliver-sdk.lisp
+++ src/screenshotbot/sdk/deliver-sdk.lisp
@@ -8,22 +8,46 @@
 
 (ql:quickload :screenshotbot.sdk)
 
+(defun output-file ()
+  (car (asdf:output-files 'asdf:compile-op (asdf:find-component :screenshotbot.sdk.deliver "deliver-sdk"))))
+
+(format t "We'll be writing to ~A~%" (output-file))
+
+(ensure-directories-exist (output-file))
+
+(defun safe-delete-file (x)
+  (when (uiop:file-exists-p x)
+    (delete-file x)))
+
 (defun deliver-main ()
- (let ((output-file (cadddr system:*line-arguments-list*)))
-   (delete-file output-file)
-   (deliver 'screenshotbot/sdk/main:main output-file 5
-             :keep-function-name t
-             :keep-debug-mode t
-             :keep-pretty-printer t
-             :keep-clos-object-printing t
-             :keep-lisp-reader t
-             ;; temporary: get the build green
-             :keep-eval t
-             :keep-symbols `(system:pipe-exit-status)
-             :packages-to-keep-symbol-names :all
-             :multiprocessing t)))
+  (let ((output-file (output-file)))
+    #-darwin ;; universal binary, output file should be temporary
+    (safe-delete-file output-file)
+    (deliver 'screenshotbot/sdk/main:main
+              output-file
+              5
+              :keep-function-name t
+              :keep-debug-mode t
+              :keep-pretty-printer t
+              :keep-clos-object-printing t
+              :keep-lisp-reader t
+              ;; temporary: get the build green
+              :keep-eval t
+              :keep-symbols `(system:pipe-exit-status)
+              :packages-to-keep-symbol-names :all
+              :multiprocessing t)))
 
 (compile 'deliver-main)
 
+#-darwin
 (deliver-main)
+
+#+darwin
+(cond
+  ((hcl:building-universal-intermediate-p)
+   (deliver-main))
+  (t
+   (safe-delete-file (output-file))
+   (hcl:save-universal-from-script "src/screenshotbot/sdk/deliver-sdk.lisp")))
+
 (uiop:quit)
