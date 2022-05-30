e95408728f844c40035ece927b4f90d61ae21b10
Arnold Noronha <arnold@tdrhq.com>
2022-05-30 10:36:33 -0400

    Bump sdk version
    GitOrigin-RevId: b7326ed57d6a9253d82023db42de9c984b164fca

--- src/screenshotbot/sdk/main.lisp
+++ src/screenshotbot/sdk/main.lisp
@@ -21,7 +21,7 @@
                               #-lispworks nil))
   (log4cl:reset-logging-configuration)
   (log:config :info)
-  (log:info "Screenshotbot SDK v2.3.4")
+  (log:info "Screenshotbot SDK v2.3.6")
   (let ((unrecognized   (parse-command-line (cdr (command-line)))))
     (when flags:*verbose*
       (log:config :debug))
