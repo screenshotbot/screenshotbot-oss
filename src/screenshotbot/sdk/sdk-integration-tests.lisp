1ab2b4ae025cf862293a5e33162b300b519f8eae
Arnold Noronha <arnold@tdrhq.com>
2022-06-13 11:42:52 -0400

    Actual runs
    GitOrigin-RevId: 10839658edc9dd0f5dbf920e75b1ac24a40308bb

--- src/screenshotbot/sdk/sdk-integration-tests.lisp
+++ src/screenshotbot/sdk/sdk-integration-tests.lisp
@@ -5,9 +5,13 @@
 
 (ql:quickload :screenshotbot.sdk.deliver)
 
+
 (defun run (cmd &rest args)
   (apply #'uiop:run-program
-           (mapcar #'namestring cmd)
+           (loop for x in cmd
+                 if (pathnamep x)
+                   collect (namestring x)
+                 else collect x)
            (append
             args
             (list :output t
@@ -17,7 +21,27 @@
                                        (asdf:find-component :screenshotbot.sdk.deliver
                                                             "deliver-sdk"))))
 
+(when (str:emptyp (uiop:getenv "SCREENSHOTBOT_API_KEY"))
+  (error "SCREENSHOTBOT_API_KEY not configured for running integration tests"))
 
 (run (list *sdk* "--help"))
 
 (assert (str:containsp "--static-website" (run (list *sdk* "--help") :output 'string)))
+
+(defmacro with-repo (&body body)
+  `(tmpdir:with-tmpdir (dir)
+     (run (list "git" "clone" "https://github.com/tdrhq/fast-example" dir))
+     (let ((*original-dir* (uiop:getcwd)))
+       (unwind-protect
+            (progn
+              (uiop:chdir dir)
+              ,@body)
+         (uiop:chdir *original-dir*)))))
+
+(with-repo
+    ;; veryfy we're correctly cloning the repo
+    (assert (path:-e "gen.sh"))
+  (run (list "./gen.sh"))
+  (run (list *sdk*
+             "--directory" "./screenshots"
+             "--production=false")))
