472572f93c8911ebe9208a634b664ff94b7da925
Arnold Noronha <arnold@tdrhq.com>
2022-06-14 17:13:44 -0400

    Clean up help message
    GitOrigin-RevId: bb3d9df70f7434b6a6326cc935a382231d1bc8e6

--- src/screenshotbot/sdk/help.lisp
+++ src/screenshotbot/sdk/help.lisp
@@ -17,12 +17,17 @@
   (format t "Usage: recorder [options]~%~%")
   (format t "Use this script from your CI pipelines or locally to
 upload screenshots and generate reports~%~%")
-  (format t "Options:~%")
+  (format t "Options:~%~%")
   (loop for (name . flag) in com.google.flag::*registered-flags* do
-    (let ((lines (str:lines (com.google.flag::help flag))))
-      (format t "--~25A~40A~%" name
+    (let* ((lines (str:lines (com.google.flag::help flag)))
+           (lines (cond
+                    ((< (length name) 23)
+                     lines)
+                    (t
+                     (list* "" lines)))))
+      (format t "  --~23A~40A~%" name
               (or (car lines) ""))
       (loop for l in (cdr lines) do
         (format t "~27A~A~%" " " (str:trim l)))))
-  (format t "~%Copyright 2020-2021 Modern Interpreters Inc.~%")
+  (format t "~%Copyright 2020-2022 Modern Interpreters Inc.~%")
   (format t "Please reach out to support@screenshotbot.io for any questions~%"))
