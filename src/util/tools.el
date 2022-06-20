ffac5a60bb248b4e1b4cde6ec4987a088f53fbdf
Arnold Noronha <arnold@tdrhq.com>
2022-06-20 16:45:05 -0400

    Add functionality to delete objects
    GitOrigin-RevId: 33409c1b344a9af5f329c5a1a22e783087d94464

--- src/util/tools.el
+++ src/util/tools.el
@@ -4,3 +4,12 @@
   (interactive)
   (let ((type (thing-at-point 'sexp)))
     (sly-inspect (format "(reverse (bknr.datastore:class-instances '%s)))" type))))
+
+(defun arnold-delete-bknr-object ()
+  (interactive)
+  (when (yes-or-no-p "Delete the BKNR object? ")
+    (sly-inspector-eval "(bknr.datastore:delete-object *)")
+    (message "Deleted.")))
+
+(define-key sly-inspector-mode-map
+  (kbd "<delete>") 'arnold-delete-bknr-object)
