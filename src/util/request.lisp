e8885b9ec156f8b8b1d55fda6158a8e126542fbd
Arnold Noronha <arnold@tdrhq.com>
2022-08-10 17:53:13 -0400

    Hacky fix to handle pipes
    Summary:
    In particular this is suggested by google fonts, even though
    it's technically not a valid URL as per the URL RFC.
    
    Differential Revision: https://phabricator.tdrhq.com/D5634
    
    GitOrigin-RevId: a89127aa0996c1d15c40528987ab58b469a7155c

--- src/util/request.lisp
+++ src/util/request.lisp
@@ -9,6 +9,12 @@
 (defun http-success-response? (response-code)
   (<= 200 response-code 204))
 
+(defun fix-bad-chars (url)
+  (cond
+    ((stringp url)
+     (str:replace-all "|" "%7C" url))
+    (t  url)))
+
 (defun http-request (url &rest args &key headers-as-hash-table
                                       want-string
                                       ensure-success
@@ -16,11 +22,12 @@
                                       (verify #-(or mswindows win32) :required
                                               #+(or mswindows win32) nil)
                      &allow-other-keys)
-  (let ((args (a:remove-from-plist args :headers-as-hash-table
-                                   :ensure-success
-                                   :want-string
-                                   #+ccl :connection-timeout
-                                   #-lispworks :read-timeout)))
+  (let* ((url (fix-bad-chars url))
+         (args (a:remove-from-plist args :headers-as-hash-table
+                                       :ensure-success
+                                       :want-string
+                                       #+ccl :connection-timeout
+                                       #-lispworks :read-timeout)))
 
     (when want-string
       (setf want-stream t))
