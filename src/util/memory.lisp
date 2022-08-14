f24dbc92aef0f73251b2037288cf341f8947d3e2
Arnold Noronha <arnold@tdrhq.com>
2022-08-14 10:43:56 -0400

    Small change
    GitOrigin-RevId: 556909dc6ba57d61bb14452fdf77b46369b4e190

--- src/util/memory.lisp
+++ src/util/memory.lisp
@@ -3,19 +3,31 @@
   (:local-nicknames (#:a #:alexandria)))
 (in-package :util/memory)
 
+(defun safe-type-of (x)
+  (let ((type (type-of x)))
+    (cond
+      ((listp type)
+       (Car type))
+      (t
+       type))))
 
-(defun histogram ()
+(defun histogram (&key by-size)
   (let ((map (make-hash-table :test #'eql))
         (sizes (make-hash-table :test #'eql)))
     (hcl:sweep-all-objects
      (lambda (obj)
-       (incf (gethash (type-of obj) map 0))
-       (incf (gethash (type-of obj) sizes 0)
+       (incf (gethash (safe-type-of obj) map 0))
+       (incf (gethash (safe-type-of obj) sizes 0)
              (hcl:find-object-size obj))))
     (let ((list (loop for k being the hash-keys in map
                         using (hash-value v)
                       collect (cons k v))))
-      (let ((ret (sort list #'> :key #'cdr)))
+      (let ((ret (sort list #'>
+                       :key (cond
+                              (by-size
+                               (lambda (x)
+                                 (gethash (car x) sizes)))
+                              (t #'cdr)))))
         (loop for (k . v) in ret
               for i below 100
               do (format t "~a ~s ~a~%" v k (gethash k sizes)))))))
@@ -37,6 +49,7 @@
         (incf curr (funcall weight-fn obj))
         (when (> curr pt)
           (return obj))))))
+
 
 ;; (histogram)
 ;; (random-sample:random-sample (objects-of-type 'system:tlatter) 100)
