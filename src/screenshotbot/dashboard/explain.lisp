2fbc7385df933831809b3c5c7f306caa4ce70f9a
Arnold Noronha <arnold@tdrhq.com>
2022-08-29 16:47:42 -0400

    Better explain icon
    Differential Revision: https://phabricator.tdrhq.com/D5684
    
    GitOrigin-RevId: 596e1bac0eef4faaa6f4b337302c4febb93ff491

--- src/screenshotbot/dashboard/explain.lisp
+++ src/screenshotbot/dashboard/explain.lisp
@@ -2,6 +2,8 @@
   (:use #:cl)
   (:import-from #:markup
                 #:deftag)
+  (:import-from #:screenshotbot/template
+                #:mdi)
   (:local-nicknames (#:a #:alexandria))
   (:export
    #:explain))
@@ -10,7 +12,8 @@
 (markup:enable-reader)
 
 (deftag explain (explanation &key title)
-  <sup>[<a data-bs-toggle= "popover" title= title
+  <sup><a data-bs-toggle= "popover" title= title
+          class= "explain-icon"
            data-bs-trigger= "focus"
            data-bs-html= "true"
-           data-bs-content= (markup:write-html <div>,@ (progn explanation)</div>) href= "#" >?</a>]</sup>)
+           data-bs-content= (markup:write-html <div>,@ (progn explanation)</div>) href= "#" ><mdi name= "help" /></a></sup>)
