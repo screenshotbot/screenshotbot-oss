dc85ea5d70d195b17a83506cbe39cdc8ad85bc14
Arnold Noronha <arnold@tdrhq.com>
2022-09-02 06:30:16 -0400

    Add tests for random-zoom-to-pixel
    Summary: Fixes T386
    
    Maniphest Tasks: T386
    
    Differential Revision: https://phabricator.tdrhq.com/D5705
    
    GitOrigin-RevId: 72a908770def18f3f6b0870b2c8b2d7cb22a71a3

--- src/screenshotbot/dashboard/test-compare.lisp
+++ src/screenshotbot/dashboard/test-compare.lisp
@@ -2,6 +2,8 @@
   (:use #:cl
         #:fiveam)
   (:import-from #:screenshotbot/dashboard/compare
+                #:random-zoom-to-on-result
+                #:image-comparison
                 #:do-image-comparison)
   (:import-from #:screenshotbot/model/image
                 #:image
@@ -15,6 +17,8 @@
                 #:delete-object)
   (:import-from #:util/store
                 #:with-test-store)
+  (:import-from #:util/testing
+                #:with-fake-request)
   (:local-nicknames (#:a #:alexandria)))
 (in-package :screenshotbot/dashboard/test-compare)
 
@@ -23,24 +27,20 @@
 (def-fixture state ()
   (with-test-store ()
    (tmpdir:with-tmpdir (dir)
-     (let ((im1 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image.png"))
-           (im2 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image-2.png"))
-           (im3 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image-3.png"))
-           (objs))
-       (labels ((save (x)
-                  (push x objs)
-                  x)
-                (make-screenshot (img)
-                  (let* ((image-blob (save (make-instance 'image-blob)))
-                         (image (save (make-instance 'screenshotbot/model:image
-                                                      :blob image-blob))))
-                    (uiop:copy-file img (blob-pathname image-blob))
-                    (save (make-instance 'screenshot
-                                          :name "foobar"
-                                          :image image)))))
-         (unwind-protect
-              (&body)
-           (loop for x in objs do (delete-object x))))))))
+     (with-fake-request ()
+       (let ((im1 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image.png"))
+             (im2 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image-2.png"))
+             (im3 (asdf:system-relative-pathname :screenshotbot "dashboard/fixture/image-3.png"))
+             (objs))
+         (labels ((make-screenshot (img)
+                    (let* ((image-blob (make-instance 'image-blob))
+                           (image (make-instance 'screenshotbot/model:image
+                                                  :blob image-blob)))
+                      (uiop:copy-file img (blob-pathname image-blob))
+                      (make-instance 'screenshot
+                                      :name "foobar"
+                                      :image image))))
+           (&body)))))))
 
 (test do-image-comparison
   (with-fixture state ()
@@ -52,3 +52,15 @@
     (uiop:with-temporary-file (:pathname out :type "png")
       (is-true
        (do-image-comparison (make-screenshot im1) (make-screenshot im2) out nil)))))
+
+(test random-zoom-to-on-result
+  (with-fixture state ()
+    (let ((imc (make-instance 'image-comparison
+                               :result (make-screenshot im1))))
+      (finishes (random-zoom-to-on-result imc))
+      (let ((res
+              (json:decode-json-from-string (random-zoom-to-on-result imc))))
+        (is (eql 360 (a:assoc-value res :width)))
+        (is (eql 360 (a:assoc-value res :height)))
+        (is (< -1 (a:assoc-value res :x) 360))
+        (is (< -1 (a:assoc-value res :y) 360))))))
