b65b3d52240e3837560353c817e5a3510c3197ea
Arnold Noronha <arnold@tdrhq.com>
2022-10-12 10:16:20 -0400

    Delete old recorder files before copying new ones
    GitOrigin-RevId: 7bc513f58b7c329b1d9a8fdf8af05203ace423d5

--- src/screenshotbot/sdk/installer.sh
+++ src/screenshotbot/sdk/installer.sh
@@ -5,10 +5,13 @@ set +e
 OUTPUT=~/screenshotbot
 RECORDER=$OUTPUT/recorder
 mkdir $OUTPUT 2>/dev/null || true
+rm -f $RECORDER
 cp sdk $RECORDER
 chmod a+x $RECORDER
 
+
 if [ -e sdk.lwheap ] ; then
+    rm -f $RECORDER.lwheap
     cp sdk.lwheap $RECORDER.lwheap
 fi
 
