08dd4ef6cbc14c2e5dde4d8870e642790d5b81af
Arnold Noronha <arnold@tdrhq.com>
2022-10-20 09:40:44 -0400

    A happy path test for schedule-snapshot
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D5943
    
    GitOrigin-RevId: e634e8c41d752ed26c38cdad92faa70fe12e4c5a

--- src/screenshotbot/testing.lisp
+++ src/screenshotbot/testing.lisp
@@ -16,20 +16,38 @@
   (:import-from #:bknr.datastore
                 #:delete-object)
   (:import-from #:screenshotbot/model/user
-                #:make-user))
+                #:make-user)
+  (:import-from #:screenshotbot/user-api
+                #:current-company
+                #:current-user)
+  (:import-from #:screenshotbot/login/common
+                #:*current-company-override*)
+  (:import-from #:util/testing
+                #:with-fake-request))
 
 (defmacro with-test-user ((&key (company (gensym "company"))
                                 (company-name "Dummy org")
                                 (company-class '(quote company))
                              (user (gensym "user"))
-                             (api-key (gensym "api-key"))) &body body)
+                             (api-key (gensym "api-key"))
+                             (logged-in-p nil)) &body body)
   `(let* ((,company (make-instance ,company-class
                                    :name ,company-name))
-          (,user (make-user :companies (list company)))
-          (,api-key (make-instance 'api-key :user user
-                                            :company company))
+          (,user (make-user :companies (list ,company)))
+          (,api-key (make-instance 'api-key :user ,user
+                                            :company ,company))
           (*events* nil))
      (declare (ignorable ,company ,user ,api-key))
-     (unwind-protect
-          (progn ,@body)
-       (delete-object ,api-key))))
+     (flet ((body ()
+              (unwind-protect
+                   (progn ,@body)
+                (delete-object ,api-key))))
+       (cond
+         (,logged-in-p
+          (with-fake-request ()
+            (auth:with-sessions ()
+              (setf (current-user) ,user)
+              (let ((*current-company-override* ,company))
+                (body)))))
+         (t
+          (body))))))
