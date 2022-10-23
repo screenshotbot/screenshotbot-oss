e8086b049fb94fed5ba5a773567abf3ee61bdb45
Arnold Noronha <arnold@tdrhq.com>
2022-10-23 13:51:13 -0400

    A sendmessage API
    Differential Revision: https://phabricator.tdrhq.com/D5976
    
    GitOrigin-RevId: 6217519f5b28e86fec142b9820bb12bfe1ed3115

--- src/screenshotbot/phabricator/builds.lisp
+++ src/screenshotbot/phabricator/builds.lisp
@@ -12,6 +12,9 @@
                 #:persistent-class)
   (:import-from #:screenshotbot/user-api
                 #:current-company)
+  (:import-from #:util/phabricator/conduit
+                #:phab-instance
+                #:call-conduit)
   (:local-nicknames (#:a #:alexandria)))
 (in-package :screenshotbot/phabricator/builds)
 
@@ -22,6 +25,8 @@
              :reader build-info-revision)
    (target-phid :initarg :target-phid
                 :reader target-phid)
+   (build-phid :initarg :build-phid
+               :reader build-phid)
    (company :initarg :company
             :index-type hash-index
             :index-reader build-infos-for-company)
@@ -31,10 +36,28 @@
   (:default-initargs :ts (get-universal-time)))
 
 (defapi (nil :uri "/phabricator/update-build" :method :post)
-        ((diff :parameter-type 'integer) (revision :parameter-type 'integer) target-phid)
+        ((diff :parameter-type 'integer) (revision :parameter-type 'integer) target-phid
+         build-phid)
   (make-instance 'build-info
                  :diff diff
                  :revision revision
                  :target-phid target-phid
+                 :build-phid build-phid
                  :company (current-company))
   "OK")
+
+(defmethod sendmessage ((phab phab-instance) (self build-info) type)
+  (let ((type (str:downcase type)))
+    (let ((args `(("buildTargetPHID" . ,(target-phid self))
+                  ("type" . ,type))))
+      (call-conduit
+       phab
+       "harbormaster.sendmessage"
+       args))))
+
+#|
+(sendmessage *phab*
+(car (reverse (bknr.datastore:class-instances 'build-info)))
+
+:fail)
+|#
