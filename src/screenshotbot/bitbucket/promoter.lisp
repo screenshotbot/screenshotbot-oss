e069bd57340a3f22f62bc24d02ff2ffeca34a2a9
Arnold Noronha <arnold@tdrhq.com>
2022-11-21 07:01:00 -0500

    The correct way to incremental rename a package
    Differential Revision: https://phabricator.tdrhq.com/D6152
    
    GitOrigin-RevId: f2c8a18ae87b468e17dca1bada69ef4dda77af7b

--- src/screenshotbot/bitbucket/promoter.lisp
+++ src/screenshotbot/bitbucket/promoter.lisp
@@ -4,8 +4,8 @@
 ;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(defpackage :screenshotbot/bitbucket/promoter
-  (:nicknames :screenshotbot/pro/bitbucket/promoter)
+(defpackage :screenshotbot/pro/bitbucket/promoter
+  (:nicknames :screenshotbot/bitbucket/promoter)
   (:use #:cl)
   (:import-from #:screenshotbot/promote-api
                 #:maybe-send-tasks
--- src/screenshotbot/bitbucket/settings.lisp
+++ src/screenshotbot/bitbucket/settings.lisp
@@ -4,8 +4,8 @@
 ;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(defpackage :screenshotbot/bitbucket/settings
-  (:nicknames :screenshotbot/pro/bitbucket/settings)
+(defpackage :screenshotbot/pro/bitbucket/settings
+  (:nicknames :screenshotbot/bitbucket/settings)
   (:use #:cl)
   (:import-from #:screenshotbot/settings-api
                 #:settings-template
