43a66a73a9d5fdde59b505d41cee694b26f85846
Arnold Noronha <arnold@tdrhq.com>
2022-12-21 16:20:52 -0500

    Add lispcalls.jar
    Summary:
    Is this still a bad idea to write a Gradle plugin in CL? It
    probably is.
    
    Differential Revision: https://phabricator.tdrhq.com/D6380
    
    GitOrigin-RevId: 9119de30572c443e7111606b8ae5188cf2015ac6

--- src/screenshotbot/plugin/src/main/java/io/screenshotbot/plugin/ScreenshotbotPlugin.java
+++ src/screenshotbot/plugin/src/main/java/io/screenshotbot/plugin/ScreenshotbotPlugin.java
@@ -1,13 +1,10 @@
 package io.screenshotbot.plugin;
 
 import org.gradle.api.*;
+import com.lispworks.LispCalls;
 
 public class ScreenshotbotPlugin implements Plugin<Project>{
     @Override
     public void apply(Project project) {
-        System.out.println("IN HERERERERER!");
-        if (true) {
-            throw new RuntimeException("shit");
-        }
     }
 }
