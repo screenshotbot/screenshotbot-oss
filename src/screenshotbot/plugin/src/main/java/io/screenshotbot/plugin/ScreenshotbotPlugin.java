79a9510c876b2a4d23a0ba95c982de1def920deb
Arnold Noronha <arnold@tdrhq.com>
2022-12-22 14:18:47 -0500

    Finally figured out how to start processing extensions
    Differential Revision: https://phabricator.tdrhq.com/D6385
    
    GitOrigin-RevId: 51aba1f85c6a134d3a64ae2d52cd24874688d535

--- src/screenshotbot/plugin/src/main/java/io/screenshotbot/plugin/ScreenshotbotPlugin.java
+++ src/screenshotbot/plugin/src/main/java/io/screenshotbot/plugin/ScreenshotbotPlugin.java
@@ -1,9 +1,16 @@
 package io.screenshotbot.plugin;
 
 import org.gradle.api.*;
+import com.android.build.gradle.api.*;
+import com.android.build.gradle.*;
 import com.lispworks.LispCalls;
 
 public class ScreenshotbotPlugin implements Plugin<Project>{
+
+    public static class Extension {
+    }
+
+    private Extension screenshotbotExtension;
 
     static {
         loadLibrary();
@@ -15,9 +22,41 @@ public class ScreenshotbotPlugin implements Plugin<Project>{
         System.out.println("Lispworks file loaded");
     }
 
+    public static void println(String str) {
+        System.out.println(str);
+    }
+
     @Override
     public void apply(Project project) {
-        System.out.println("just before running stuff");
-        System.out.println("got length: " + LispCalls.callIntV("CL-USER::SIMPLE-TEST", "foobar"));
+        var extensions = project.getExtensions();
+
+        screenshotbotExtension = extensions.create("screenshotbot",
+                                                   Extension.class);
+
+        var plugins = project.getPlugins();
+        if (plugins.hasPlugin("com.facebook.testing.screenshot")) {
+            extensions.configure(TestedExtension.class,
+                                 (androidExtension) -> {
+                                     generateFacebookTasks(androidExtension);
+                                 });
+        }
+    }
+
+    private TestedExtension getProjectExtension(Project project) {
+        var extensions = project.getExtensions();
+        var plugins = project.getPlugins();
+        if (plugins.hasPlugin("com.android.application")) {
+            return extensions.findByType(AppExtension.class);
+        } else if (plugins.hasPlugin("com.android.library")) {
+            return extensions.findByType(LibraryExtension.class);
+        } else {
+            throw new IllegalArgumentException("Screenshotbot plugin requires either an Android Application or Android Library plugin");
+        }
+    }
+
+    public void generateFacebookTasks(TestedExtension androidExtension) {
+        androidExtension.getTestVariants().all((variant) -> {
+                println("variant: " + variant.toString());
+            });
     }
 }
