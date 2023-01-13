8f327c535aa26f0a0bfd7e18b535e4b6b8abeede
Arnold Noronha <arnold@tdrhq.com>
2023-01-12 21:24:14 -0500

    Remove tabs...
    Summary: this is annoying in code review
    
    Differential Revision: https://phabricator.tdrhq.com/D6483
    
    GitOrigin-RevId: d50e4df8b4807947d2f2c674d96b0ef4a2c5b2e5

--- src/screenshotbot/magick/magick-native.c
+++ src/screenshotbot/magick/magick-native.c
@@ -18,48 +18,49 @@ typedef struct _pixel {
 #define IF7(v1,v2) v2
 #endif
 
+
 #define MAX_QUANTUM ((1 << MAGICKCORE_QUANTUM_DEPTH) - 1)
 
 extern int screenshotbot_verify_magick(CompositeOperator srcCompositeOp,
-									   IF7(AlphaChannelOption,AlphaChannelType) onAlphaChannel) {
-		size_t depth;
-		GetMagickQuantumDepth(&depth);
-		if (MAGICKCORE_QUANTUM_DEPTH != depth) {
-				return -1;
-		}
+                                       IF7(AlphaChannelOption,AlphaChannelType) onAlphaChannel) {
+        size_t depth;
+        GetMagickQuantumDepth(&depth);
+        if (MAGICKCORE_QUANTUM_DEPTH != depth) {
+                return -1;
+        }
 
-		if (srcCompositeOp != SrcCompositeOp) {
-				return -2;
-		}
+        if (srcCompositeOp != SrcCompositeOp) {
+                return -2;
+        }
 
-		if (onAlphaChannel != SetAlphaChannel) {
-				return -3;
-		}
+        if (onAlphaChannel != SetAlphaChannel) {
+                return -3;
+        }
 
-		const char* features = GetMagickFeatures();
-		char* feat = strstr(features, "HDRI");
+        const char* features = GetMagickFeatures();
+        char* feat = strstr(features, "HDRI");
 
 
 #if MAGICKCORE_HDRI_ENABLE
-		int hdriMatches = (feat != NULL);
+        int hdriMatches = (feat != NULL);
 #else
-		int hdriMatches = (feat == NULL);
+        int hdriMatches = (feat == NULL);
 #endif
-		if (!hdriMatches) {
-				printf("HDRI setting does not match, original: %d. Features `%s`",
-					   MAGICKCORE_HDRI_ENABLE,
-					   features);
-				return -4;
-		}
+        if (!hdriMatches) {
+                printf("HDRI setting does not match, original: %d. Features `%s`",
+                       MAGICKCORE_HDRI_ENABLE,
+                       features);
+                return -4;
+        }
 
-		return 1;
+        return 1;
 }
 
 struct mask {
-		size_t x;
-		size_t y;
-		size_t width;
-		size_t height;
+        size_t x;
+        size_t y;
+        size_t width;
+        size_t height;
 };
 
 /*
@@ -67,51 +68,51 @@ struct mask {
  */
 extern MagickBooleanType
 screenshotbot_set_pixel(
-		MagickWand *wand,
-		const pixel *ppixel, // pointer just for FLI simplicity
-		const char* color) {
-		PixelIterator *iterator = NewPixelIterator(wand);
-		MagickBooleanType ret = MagickFalse;
-		size_t height = MagickGetImageHeight(wand);
-		pixel pixel = *ppixel;
+        MagickWand *wand,
+        const pixel *ppixel, // pointer just for FLI simplicity
+        const char* color) {
+        PixelIterator *iterator = NewPixelIterator(wand);
+        MagickBooleanType ret = MagickFalse;
+        size_t height = MagickGetImageHeight(wand);
+        pixel pixel = *ppixel;
 
-		if (pixel.y >= height) {
-				printf("Invalid height: %zu\n", pixel.y);
-				goto cleanup;
-		}
+        if (pixel.y >= height) {
+                printf("Invalid height: %zu\n", pixel.y);
+                goto cleanup;
+        }
 
-		size_t width;
-		PixelWand** row;
-		for (int y = 0; y <= pixel.y; y++) {
-				row = PixelGetNextIteratorRow(iterator, &width);
-		}
+        size_t width;
+        PixelWand** row;
+        for (int y = 0; y <= pixel.y; y++) {
+                row = PixelGetNextIteratorRow(iterator, &width);
+        }
 
-		if (pixel.x >= width) {
-				printf("Invalid width: %zu out of %zu\n", pixel.x, width);
-				goto cleanup;
-		}
+        if (pixel.x >= width) {
+                printf("Invalid width: %zu out of %zu\n", pixel.x, width);
+                goto cleanup;
+        }
 
-		ret = PixelSetColor(row[pixel.x], color);
-		if (!ret) {
-				printf("PixelSetColor failed\n");
-				goto cleanup;
-		}
+        ret = PixelSetColor(row[pixel.x], color);
+        if (!ret) {
+                printf("PixelSetColor failed\n");
+                goto cleanup;
+        }
 
-		ret = PixelSyncIterator(iterator);
+        ret = PixelSyncIterator(iterator);
 cleanup:
-		DestroyPixelIterator(iterator);
-		return ret;
+        DestroyPixelIterator(iterator);
+        return ret;
 }
 
 static int _mask_cmp(const void* p1, const void* p2) {
-		const struct mask* m1 = (struct mask*) p1;
-		const struct mask* m2 = (struct mask*) p2;
+        const struct mask* m1 = (struct mask*) p1;
+        const struct mask* m2 = (struct mask*) p2;
 
-		return m1->x - m2->x;
+        return m1->x - m2->x;
 }
 
 static int in_range(int x, int start, int width) {
-		return start <= x && x < (start + width);
+        return start <= x && x < (start + width);
 }
 
 /*
@@ -119,20 +120,20 @@ static int in_range(int x, int start, int width) {
  * positions. The number that is returned is number of elements in output.
  */
 static int filter_masks(struct mask* masks, struct mask* output, int numMasks, int y) {
-		int ret = 0;
-		for (int i = 0; i < numMasks; i++) {
-				struct mask mask = masks[i];
-				if (in_range(y, mask.y, mask.height)) {
-						output[ret++] = mask;
-				}
-		}
+        int ret = 0;
+        for (int i = 0; i < numMasks; i++) {
+                struct mask mask = masks[i];
+                if (in_range(y, mask.y, mask.height)) {
+                        output[ret++] = mask;
+                }
+        }
 
-		qsort(output, ret, sizeof(struct mask), &_mask_cmp);
-		return ret;
+        qsort(output, ret, sizeof(struct mask), &_mask_cmp);
+        return ret;
 }
 
 inline int _max(int a, int b) {
-		return (a > b ? a : b);
+        return (a > b ? a : b);
 }
 
 extern size_t
@@ -140,42 +141,42 @@ screenshotbot_find_non_transparent_pixels_with_masks
 (MagickWand* wand, struct mask* masks, size_t numMasks, pixel* output, size_t max) {
         max--;
         PixelIterator* iterator = NewPixelIterator(wand);
-		struct mask *tmp = malloc(numMasks * sizeof(struct mask));
+        struct mask *tmp = malloc(numMasks * sizeof(struct mask));
 
-		//printf("The top mask is: %zu, %zu\n", masks[0].y, masks[0].height);
+        //printf("The top mask is: %zu, %zu\n", masks[0].y, masks[0].height);
 
         size_t ret = 0;
         size_t height = MagickGetImageHeight(wand);
         for (int y = 0; y < height; ++y) {
-				int numLocalMasks = filter_masks(masks, tmp, numMasks, y);
-				//printf("For %d, got %d local masks out of %zu\n", y, numLocalMasks, numMasks);
-				int nextMask = 0;
+                int numLocalMasks = filter_masks(masks, tmp, numMasks, y);
+                //printf("For %d, got %d local masks out of %zu\n", y, numLocalMasks, numMasks);
+                int nextMask = 0;
 
                 size_t width = 0;
                 PixelWand** row = PixelGetNextIteratorRow(iterator, &width);
 
                 for (int x = 0; x < width; x++) {
 
-						if (nextMask < numLocalMasks) {
-								if (x >= tmp[nextMask].x) {
-										x = _max(x, tmp[nextMask].x + tmp[nextMask].width) - 1;
-										nextMask++;
-										//printf("Set x to %d\n", x);
-										continue;
-								}
-						}
+                        if (nextMask < numLocalMasks) {
+                                if (x >= tmp[nextMask].x) {
+                                        x = _max(x, tmp[nextMask].x + tmp[nextMask].width) - 1;
+                                        nextMask++;
+                                        //printf("Set x to %d\n", x);
+                                        continue;
+                                }
+                        }
 
                         Quantum px = PixelGetAlphaQuantum(row[x]);
 
-						/*
-						 * Currently the yellow that's drawn as part
-						 * of the mask is ~204.  However, we also need
-						 * to deal with a situation that the yellow is
-						 * overlaid on the red pixel. So even though
-						 * this function is named "get_non_alpha",
-						 * it's actually "get_non_alpha" where green
-						 * is 0.
-						 */
+                        /*
+                         * Currently the yellow that's drawn as part
+                         * of the mask is ~204.  However, we also need
+                         * to deal with a situation that the yellow is
+                         * overlaid on the red pixel. So even though
+                         * this function is named "get_non_alpha",
+                         * it's actually "get_non_alpha" where green
+                         * is 0.
+                         */
                         if (px == MAX_QUANTUM && PixelGetGreenQuantum(row[x]) == 0) {
                                 output[ret].x = x;
                                 output[ret].y = y;
@@ -188,17 +189,17 @@ screenshotbot_find_non_transparent_pixels_with_masks
                 }
         }
 
-        cleanup:
-		free(tmp);
+cleanup:
+        free(tmp);
         DestroyPixelIterator(iterator);
         return ret;
 }
 
 extern size_t screenshotbot_find_non_transparent_pixels(MagickWand* wand, pixel* output, size_t max) {
-		return screenshotbot_find_non_transparent_pixels_with_masks(
-				wand,
-				NULL,
-				0,
-				output,
-				max);
+        return screenshotbot_find_non_transparent_pixels_with_masks(
+                wand,
+                NULL,
+                0,
+                output,
+                max);
 }
