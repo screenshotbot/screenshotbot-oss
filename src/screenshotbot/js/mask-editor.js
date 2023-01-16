0cd946deed137cc0524edbf725a131a67607ac8e
Arnold Noronha <arnold@tdrhq.com>
2023-01-16 13:46:24 -0500

    Add Panning
    Differential Revision: https://phabricator.tdrhq.com/D6509
    
    GitOrigin-RevId: f975c8f826f739110bc5100c5948c1a0c6fb32dd

--- src/screenshotbot/js/mask-editor.js
+++ src/screenshotbot/js/mask-editor.js
@@ -73,12 +73,14 @@ $(function () {
             return fabric.util.transformPoint(point, inverse);
         }
 
-        canvas.on('mouse:down', function (e) {
-            console.log(e);
-            var point = getOriginalPoint(e.pointer);
-            startX = point.x;
-            startY = point.y;
-            isDragging = true;
+        canvas.on('mouse:down', function (opt) {
+            if (opt.e.altKey !== true) {
+                console.log(opt);
+                var point = getOriginalPoint(opt.pointer);
+                startX = point.x;
+                startY = point.y;
+                isDragging = true;
+            }
         });
 
         function addRect(props) {
@@ -173,5 +175,38 @@ $(function () {
             opt.e.stopPropagation();
         });
 
+        enablePanningOnCanvas(canvas);
     }
 });
+
+
+function enablePanningOnCanvas(canvas) {
+    canvas.on('mouse:down', function(opt) {
+        var evt = opt.e;
+        if (evt.altKey === true) {
+            this.isDragging = true;
+            this.selection = false;
+            this.lastPosX = evt.clientX;
+            this.lastPosY = evt.clientY;
+        }
+    });
+    canvas.on('mouse:move', function(opt) {
+        if (this.isDragging) {
+            var e = opt.e;
+            var vpt = this.viewportTransform;
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+        }
+    });
+    canvas.on('mouse:up', function(opt) {
+        // on mouse up we want to recalculate new interaction
+        // for all objects, so we call setViewportTransform
+        this.setViewportTransform(this.viewportTransform);
+        this.isDragging = false;
+        this.selection = true;
+    });
+
+}
