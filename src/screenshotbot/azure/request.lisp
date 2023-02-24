87f5f4746f744a6e2b39373326a25bd0bbe92ccf
Arnold Noronha <arnold@tdrhq.com>
2023-02-24 13:19:55 -0500

    Figured out the Azure API, yay
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D6738
    
    GitOrigin-RevId: e56b040e9ac6e359b390717e8f144be15f8bd2d1

--- src/screenshotbot/azure/request.lisp
+++ src/screenshotbot/azure/request.lisp
@@ -30,10 +30,47 @@ https://learn.microsoft.com/en-us/rest/api/azure/devops/build/status/get?view=az
 (defclass build-definition-reference ()
   ((name :json-key "name"
          :json-type :string
+         :initarg :name
          :reader name)
    (id :json-key "id"
        :json-type :number
-       :reader id))
+       :reader id)
+   (description :json-key "description"
+                :json-type (or null :string)
+                :initarg :description)
+   (repository :json-key "repository"
+               :json-type (or null build-repository)
+               :initarg :repository))
+  (:metaclass ext-json-serializable-class))
+
+(defclass git-status-context ()
+  ((genre :initform "screenshotbot"
+          :json-key "genre"
+          :json-type :string)
+   (name :initarg :name
+         :json-key "name"
+         :json-type :string))
+  (:metaclass ext-json-serializable-class))
+
+(defclass pull-request-status ()
+  ((description :json-key "description"
+                :json-type :string
+                :initarg :description)
+   (state :json-key "state"
+          :json-type :string
+          :initarg :state)
+   (context :initarg :context
+            :json-key "context"
+            :json-type git-status-context)
+   (target-url :initarg :target-url
+               :json-key "targetUrl"
+               :json-type :string))
+  (:metaclass ext-json-serializable-class))
+
+(defclass build-repository ()
+  ((name :json-type :string
+         :json-key "name"
+         :initarg :name))
   (:metaclass ext-json-serializable-class))
 
 
@@ -51,28 +88,39 @@ https://learn.microsoft.com/en-us/rest/api/azure/devops/build/status/get?view=az
                                            :organization "testsbot"
                                            :project "fast-example"))
 
+(define-condition azure-error (simple-error)
+  ((headers :initarg :headers)))
+
 (defun azure-request (azure url &key
                                   method
                                   response-type
+                                  parameters
                                   content)
-  (multiple-value-bind (response code)
-      (http-request
-       (format nil "https://dev.azure.com/~a/~a/_apis/~a?api-version=7.0"
-               (organization azure)
-               (project azure)
-               url)
-       :content (with-output-to-string (out)
-                  (yason:encode content out))
-       :content-type "application/json"
-       :basic-authorization (list "" (token azure))
-       :accept "application/json"
-       :method method
-       :want-string t)
-    (cond
-      ((<= 400 code 510)
-       (format t "Got failure: ~a" response))
-      (t
-       (json-mop:json-to-clos response response-type)))))
+  (declare (optimize (speed 0) (debug 3)))
+  (let ((content (with-output-to-string (out)
+                   (yason:encode content out))))
+    (log:info "Sending body: ~a" content)
+    (multiple-value-bind (response code headers)
+       (http-request
+        (format nil "https://dev.azure.com/~a/~a/_apis/~a?api-version=7.0"
+                (organization azure)
+                (project azure)
+                url)
+        :content content
+        :content-type "application/json"
+        :basic-authorization (list "" (token azure))
+        :accept "application/json"
+        :parameters parameters
+        :method method
+        :want-string t)
+     (cond
+       ((<= 400 code 510)
+        (error 'azure-error
+               :headers headers
+               :format-control "Got failure: ~a"
+               :format-arguments (list response)))
+       (t
+        (json-mop:json-to-clos response response-type))))))
 
 (defun queue-build (azure &key
                       uri
@@ -84,7 +132,8 @@ https://learn.microsoft.com/en-us/rest/api/azure/devops/build/status/get?view=az
      azure
      "build/builds"
      :method :post
-     :content body)))
+     :content body
+     :parameters `(("sourceBuildId" . "5")))))
 
 (defclass list-definitions-response ()
   ((value :json-type (:list build-definition-reference)
@@ -99,6 +148,52 @@ https://learn.microsoft.com/en-us/rest/api/azure/devops/build/status/get?view=az
    :method :get
    :response-type 'list-definitions-response))
 
+(defun create-definition (azure build-definition)
+  (azure-request
+   azure
+   "build/definitions"
+   :method :post
+   :response-type 'build-definition-reference
+   :content build-definition))
 
+(defmethod create-pull-request-status (azure status
+                                       &key repository-id
+                                         pull-request-id)
+  (declare (optimize (speed 0) (debug 3)))
+  (azure-request
+   azure
+   (format nil
+           "git/repositories/~a/pullRequests/~a/statuses"
+           repository-id
+           pull-request-id)
+   :method :post
+   :response-type 'pull-request-status
+   :content status))
+
+#|
+https://learn.microsoft.com/en-us/azure/devops/repos/git/branch-policies?view=azure-devops&tabs=browser#build-validation
+|#
+
+#+nil
+(queue-build *test-azure*)
+
+#+nil
+(create-pull-request-status
+ *test-azure*
+ (make-instance 'pull-request-status
+                :description "some stuff"
+                :state "failed"
+                :target-url "https://screenshotbot.io/runs"
+                :context (make-instance 'git-status-context
+                                        :name "foobar"))
+ :repository-id "fast-example"
+ :pull-request-id 1)
+
+#+nil
+(create-definition *test-azure*
+                   (make-instance 'build-definition-reference
+                                  :name "test-foo"
+                                  :description "this is a sample job"
+                                  :))
 #+nil
 (format t "~a"(list-definitions *test-azure*))
