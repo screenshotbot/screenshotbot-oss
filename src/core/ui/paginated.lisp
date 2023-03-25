04dc5bf216f0d5b73abcbe4a9cba62224cde104d
Arnold Noronha <arnold@tdrhq.com>
2023-03-25 10:53:02 -0400

    Break up pagination-helper for testability
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D6852
    
    GitOrigin-RevId: 8116e0007a029acd83000664c7f7e6b239c9eb2e

--- src/core/ui/paginated.lisp
+++ src/core/ui/paginated.lisp
@@ -8,59 +8,73 @@
   (:use #:cl)
   (:import-from #:nibble
                 #:nibble)
+  (:import-from #:alexandria
+                #:remove-from-plist)
   (:export
    #:paginated))
 (in-package :core/ui/paginated)
 
 (markup:enable-reader)
 
-(defun paginated (fn &key
-                       (filter #'identity)
-                       (empty-view)
-                       (num 24)
-                       (items nil)
-                       (start-counter 0)
-                       (iterator nil)
-                       (pass-index-p nil))
+(defun pagination-helper (&key
+                           (filter #'identity)
+                           (empty-view)
+                           (num 24)
+                           (items nil)
+                           (start-counter 0)
+                           (iterator nil)
+                           renderer)
+  "The renderer here is a function that takes three arguments: the list of objects to render,
+ a lambda that calls pagination-helper on the remaining objects with
+ the same arguments, and the start-counter of the first element.
+
+The pagination-helper doesn't handle rendering on its own, for testability purposes."
+  (multiple-value-bind (this-page rest)
+      (cond
+        (iterator
+         (loop for i from 0 upto num
+               for next = (funcall iterator)
+               if next
+                 collect next into results
+               if (null next)
+                 do (return (values results nil))
+               finally
+                  (return (values results t))))
+        (t
+         (util/lists:head items num :filter filter)))
+    (cond
+      (this-page
+       (let* ((load-more (when rest
+                           (lambda ()
+                             (pagination-helper
+                              :num num :items (if iterator nil rest)
+                              :start-counter (+ start-counter (length this-page))
+                              :iterator iterator
+                              :filter filter
+                              :renderer renderer)))))
+         (funcall renderer this-page load-more start-counter)))
+      (t
+       empty-view))))
+
+(defun paginated (fn &rest args &key pass-index-p &allow-other-keys)
   "Creates a paginated view. If no element matches, then we return
 NIL, which can be used as a way of determining whether to render an
 empty message."
-  (assert (functionp fn))
   (let ((fn (if pass-index-p
                 fn
                 (lambda (name i)
                   (declare (ignore i))
                   (funcall fn name)))))
-   (multiple-value-bind (this-page rest)
-       (cond
-         (iterator
-          (loop for i from 0 upto num
-                for next = (funcall iterator)
-                if next
-                  collect next into results
-                if (null next)
-                  do (return (values results nil))
-                finally
-                   (return (values results t))))
-         (t
-          (util/lists:head items num :filter filter)))
-     (cond
-       (this-page
-        (let* ((load-more (nibble (:name :load-more)
-                            (paginated fn :num num :items (if iterator nil rest)
-                                          :start-counter (+ start-counter (length this-page))
-                                          :iterator iterator
-                                          :pass-index-p t
-                                          :filter filter))))
-          <div class= "row pb-4 load-more-container" >
-            ,@(loop for page in this-page
-                    for i from start-counter
-                    collect (funcall fn page i))
+   (apply #'pagination-helper
+          :renderer (lambda (this-page load-more start-counter)
+                      <div class= "row pb-4 load-more-container" >
+                      ,@(loop for page in this-page
+                              for i from start-counter
+                              collect (funcall fn page i))
 
-          ,(when rest
-             <div class= "col-12 d-flex justify-content-center">
-             <button class= "btn btn-primary load-more-button" data-load-more=load-more >Load More</button>
-             </div>)
-          </div>))
-       (t
-        empty-view)))))
+                      ,(when load-more
+                         <div class= "col-12 d-flex justify-content-center">
+                         <button class= "btn btn-primary load-more-button" data-load-more= (nibble (:name :load-more) (funcall load-more)) >Load More</button>
+                         </div>)
+                      </div>)
+          (remove-from-plist args :pass-index-p))))
