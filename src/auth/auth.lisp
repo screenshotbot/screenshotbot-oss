5f63c851a583fda3ff95e640d630a11e8fc9f620
Arnold Noronha <arnold@tdrhq.com>
2023-06-30 17:37:07 -0400

    Use the new index to pull out user-session-values
    Differential Revision: https://phabricator.tdrhq.com/D7407
    
    GitOrigin-RevId: 3c345addaf240a49e484bedddaf70869cba41641

--- src/auth/auth.lisp
+++ src/auth/auth.lisp
@@ -15,10 +15,11 @@
 (with-class-validation
  (defclass user-session-value (store-object)
    ((session-key-and-prop-key
-     :accessor session-key-and-prop-key)
+     :accessor session-key-and-prop-key
+     :documentation "This is an older mechanism used to find the cookies. Deprecated. It's only here for OSS support.")
     (prop-key :initarg :prop-key
               :accessor prop-key
-              :documentation "The property key. e.g. :USER, or :COMPANY. Note: this is currently not being used.")
+              :documentation "The property key. e.g. :USER, or :COMPANY.")
     (session-token :initarg :session-token
                    :accessor  session-token
                    :index +sessions-index+
@@ -33,7 +34,7 @@
    (:metaclass persistent-class)))
 
 (defmethod initialize-instance :after ((uv user-session-value)
-                                       &key session-key
+                                       &key session-key #| deprecated |#
                                          prop-key
                                          value
                                          session-token
@@ -43,20 +44,14 @@
         (cons session-key prop-key)))
 
 
-(defvar *cache* (make-hash-table :test #'equal))
 
-(defun find-user-session-value (session-key prop-key)
-  "The reason we don't use a BKNR index for this is because the
-index has a tendency to go stale. When the index does go stale,
-sessions are one of the first objects created that can make the index
-incorrect. We don't have a crazy number of sessions per instance that
-this would be a problem."
-  (let ((cache-key (cons session-key prop-key)))
-    (util/misc:or-setf
-     (gethash cache-key *cache*)
-     (loop for user-session-value in (bknr.datastore:store-objects-with-class 'user-session-value)
-           if (equal cache-key (session-key-and-prop-key user-session-value))
-             return user-session-value))))
+(defun find-user-session-value (token domain prop-key)
+  (let ((usvs (values-for-session-token token)))
+    (fset:do-set (usv usvs)
+      (when (and
+             (eql prop-key (prop-key usv))
+             (equal domain (session-domain usv)))
+        (return-from find-user-session-value usv)))))
 
 (defvar *hash-cache* (make-hash-table :test #'equal))
 
@@ -202,7 +197,8 @@ value."
   *current-session*)
 
 (defun session-value (key &key (session (current-session)))
-  (let ((x (find-user-session-value (session-key session)
+  (let ((x (find-user-session-value (%session-token session)
+                                    (session-domain session)
                                     key)))
     (and x
          (value x))))
@@ -211,12 +207,13 @@ value."
 (defun (setf session-value) (value key &key (session (current-session)))
   (let ((x (bt:with-lock-held (*lock*)
              (or
-              (find-user-session-value (session-key session)
+              (find-user-session-value (%session-token session)
+                                       (session-domain session)
                                        key)
               (make-instance 'user-session-value
                              :session-token (%session-token session)
                              :session-domain (session-domain session)
-                             :session-key (session-key session)
+                             :session-key (session-key session) #| deprecated |#
                              :prop-key key)))))
     (bknr.datastore:with-transaction ()
       (setf (value x) value))))
