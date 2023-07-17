a188427e8e85cc8fb7c87e082559bed385766d8e
Arnold Noronha <arnold@tdrhq.com>
2023-07-17 15:09:22 -0400

    Delete some unused code
    Summary:
    See D7524, and D7523 that made this obsolete. In particular
    the with-global-kernel macro is unreliable in tests.
    
    Differential Revision: https://phabricator.tdrhq.com/D7525
    
    GitOrigin-RevId: 7e4b557c9be153e3aecb740c2b6d2f48706a8647

--- src/util/tests/test-lparallel.lisp
+++ src/util/tests/test-lparallel.lisp
@@ -7,11 +7,7 @@
 (defpackage :util/tests/test-lparallel
   (:use #:cl
         #:fiveam)
-  (:import-from #:util/lparallel
-                #:while-promises
-                #:*sleep-time*
-                #:wait-for-many
-                #:bind-future)
+  (:import-from #:util/lparallel)
   (:import-from #:util/testing
                 #:with-global-kernel)
   (:import-from #:lparallel.promise
@@ -21,40 +17,3 @@
 (in-package :util/tests/test-lparallel)
 
 (util/fiveam:def-suite)
-
-(test simple-bind-fn
-  (with-global-kernel ()
-    (let ((f1 (future 2)))
-      (is (equal 3 (force (bind-future f1 (lambda (x) (+ 1 x))))))
-      (is (equal 3 (force (bind-future f1 (lambda (x) (+ 1 x)))))))))
-
-(test wait-for-many
-  (with-global-kernel ()
-    (let ((f1 (future 2))
-          (p1 (lparallel:promise)))
-      (is (eql f1 (wait-for-many (list f1 p1))))
-      (is (eql f1 (wait-for-many (list f1)))))))
-
-(test while-promises-happy-path
-  (let ((*sleep-time* 0.1))
-   (with-global-kernel ()
-     (let ((f1 (future 2))
-           (p1 (lparallel:promise)))
-       (force f1)
-       (let ((results))
-         (future
-           (while-promises (f1 p1)
-             (push (list (lparallel:fulfilledp f1)
-                         (lparallel:fulfilledp p1))
-                   results)))
-         (loop until results
-               for i from 0 to 100
-               do (sleep 0.1))
-         (lparallel:fulfill p1 10)
-         (loop until (eql 2 (length results))
-               for i from 0 to 100
-               do (sleep 0.1))
-         (is (equal
-              `((t t)
-                (t nil))
-              results)))))))
