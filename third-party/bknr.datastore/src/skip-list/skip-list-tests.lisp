19c062da38ccc707f5aab51eea9853e1348a039f
Arnold Noronha <arnold@tdrhq.com>
2023-07-22 12:27:32 -0400

    Add bknr.skip-list tests to CI
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D7553
    
    GitOrigin-RevId: 891145dbee4896790a5636447e575abe6c89a5f9

--- third-party/bknr.datastore/src/skip-list/skip-list-tests.lisp
+++ third-party/bknr.datastore/src/skip-list/skip-list-tests.lisp
@@ -1,15 +1,30 @@
-(in-package :bknr.skip-list)
+(defpackage :bknr.skip-list-tests
+  (:use #:cl
+        #:fiveam
+        #:bknr.skip-list)
+  (:import-from #:bknr.skip-list
+                #:make-node
+                #:node-level
+                #:node-forward
+                #:skip-list-to-list))
+(in-package :bknr.skip-list-tests)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (use-package :unit-test))
-
-(define-test-class skip-list-test-class)
+(def-suite* :bknr.skip-list)
 
 (defmacro define-skip-list-test (name &rest body)
-  `(make-instance 'skip-list-test-class
-    :unit :skip-list
-    :name ,name
-    :body #'(lambda () ,@body)))
+  `(test ,(intern (string name))
+     ,@body))
+
+(defmacro test-equal (&rest args)
+  `(is (equal ,@args)))
+
+(defmacro test-assert (&rest args)
+  `(is-true ,@args))
+
+(defmacro test-condition (expr (%quote condition))
+  (declare (ignore %quote))
+  `(signals ,condition
+     ,expr))
 
 (define-skip-list-test "Node test"
     (let ((node (make-node 0 0 23)))
@@ -26,7 +41,9 @@
 	(let ((value (random 10000))
 	      (key (random 10000)))
 	  (skip-list-insert sl key value)
-	  (test-equal (skip-list-search sl key) value)))))
+      ;; Avoiding fiveam here for the noise
+	  (assert (equal (skip-list-search sl key) value)))))
+  (pass))
 
 (define-skip-list-test "Hashcompare"
     (let ((sl (make-instance 'skip-list))
@@ -37,25 +54,26 @@
 	  (setf (gethash key hash) value)
 	  (skip-list-insert sl key value)))
       (loop for key being the hash-key of hash
-	    do (test-equal (gethash key hash)
-			   (skip-list-search sl key)))))
+	    do (assert (equal (gethash key hash)
+                          (skip-list-search sl key)))))
+  (pass))
 
 (define-skip-list-test "Length of skiplist"
     (let ((sl (make-instance 'skip-list)))
       (dotimes (i 100)
-	(skip-list-insert sl i i)
-	(test-equal (skip-list-search sl i) i)
-	(test-equal (skip-list-search sl (1+ i) :not-found) :not-found)
-	(dotimes (j i)
-	  (test-equal (skip-list-search sl j) j))
-	(test-equal (skip-list-length sl) (1+ i)))
+	    (skip-list-insert sl i i)
+	    (test-equal (skip-list-search sl i) i)
+	    (test-equal (skip-list-search sl (1+ i) :not-found) :not-found)
+	    (dotimes (j i)
+	      (assert (equal (skip-list-search sl j) j)))
+	    (test-equal (skip-list-length sl) (1+ i)))
       (dotimes (i 100)
-	(test-equal (skip-list-length sl) (- 100 i))
-	(skip-list-delete sl i)
-	(test-equal (skip-list-length sl) (- 100 (1+ i)))
-	(test-equal (skip-list-search sl i :not-found) :not-found)
-	(loop for j from (1+ i) to 99
-	      do (test-equal (skip-list-search sl j) j)))
+	    (test-equal (skip-list-length sl) (- 100 i))
+	    (skip-list-delete sl i)
+	    (test-equal (skip-list-length sl) (- 100 (1+ i)))
+	    (test-equal (skip-list-search sl i :not-found) :not-found)
+	    (loop for j from (1+ i) to 99
+	          do (assert (equal (skip-list-search sl j) j))))
       (test-equal (skip-list-length sl) 0)
       (test-equal (skip-list-to-list sl) nil)))
 
@@ -71,4 +89,3 @@
 ;; 	    (dotimes (i 100000)
 ;; 	      (skip-list-search sl i))))
 ;;     (prof:show-flat-profile)))
-
