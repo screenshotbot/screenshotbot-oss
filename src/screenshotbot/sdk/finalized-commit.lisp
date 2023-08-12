f3a7a42dade21b3f2df2f9970cbd90c9ee4c3955
Arnold Noronha <arnold@tdrhq.com>
2023-08-12 18:04:52 -0400

    Add finalize to clingon
    Differential Revision: https://phabricator.tdrhq.com/D7727
    
    GitOrigin-RevId: 72a7e7a9dc3f9545daae88b533ca850274e8532e

--- src/screenshotbot/sdk/finalized-commit.lisp
+++ src/screenshotbot/sdk/finalized-commit.lisp
@@ -11,18 +11,29 @@
                 #:git-repo)
   (:import-from #:screenshotbot/sdk/api-context
                 #:remote-version)
+  (:import-from #:screenshotbot/sdk/cli-common
+                #:*root-commands*
+                #:with-clingon-api-context)
+  (:import-from #:clingon.command
+                #:getopt)
+  (:import-from #:alexandria
+                #:assoc-value)
+  (:import-from #:clingon.options
+                #:make-option)
   (:local-nicknames (#:flags #:screenshotbot/sdk/flags)
                     (#:sdk #:screenshotbot/sdk/sdk)
                     (#:version-check #:screenshotbot/sdk/version-check)
                     (#:dto #:screenshotbot/api/model)))
 (in-package :screenshotbot/sdk/finalized-commit)
 
-(defun finalize-commit (api-context)
+(defun finalize-commit (api-context &key commit)
   (when (< (remote-version api-context) 7)
     (error "The remote Screenshotbot server does not support --mark-unchanged-from"))
 
   (let* ((repo (git-repo))
-         (commit (current-commit repo)))
+         (commit (or
+                  commit
+                  (current-commit repo))))
     (sdk:request api-context
                  "/api/unchanged-run"
                  :method :post
@@ -30,3 +41,21 @@
                  (make-instance 'dto:finalized-commit
                                 :commit commit))
     (log:info "Finalized runs for commit ~a" commit)))
+
+(defun finalize-commit/command ()
+  (clingon:make-command
+   :name "finalize"
+   :description "'Finalize' a commit. This tells Screenshotbot that there will no more runs associated with this commit (even if the same commit exists in multiple forked repositories in the organization"
+   :options (list
+             (make-option
+              :string
+              :long-name "commit"
+              :initial-value nil
+              :description "The commit to finalize, but will default to the current commit on the current repository"
+              :key :commit))
+   :handler (lambda (cmd)
+              (with-clingon-api-context (api-context cmd)
+                (finalize-commit api-context
+                                 :commit (getopt cmd :commit))))))
+
+(setf (assoc-value *root-commands* 'finalize) #'finalize-commit/command)
