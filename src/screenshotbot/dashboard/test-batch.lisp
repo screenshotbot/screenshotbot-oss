d26a8b585681471d56785334dae37fcab3bd4dff
Arnold Noronha <arnold@tdrhq.com>
2023-08-14 07:08:43 -0400

    Boilerplate for batch dashboard
    Maniphest Tasks: T733
    
    Differential Revision: https://phabricator.tdrhq.com/D7763
    
    GitOrigin-RevId: f2e703dab7120294c790fc0b0e9077144e236a20

---
+++ src/screenshotbot/dashboard/test-batch.lisp
@@ -0,0 +1,13 @@
+;;;; Copyright 2018-Present Modern Interpreters Inc.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+(defpackage :screenshotbot/dashboard/test-batch
+  (:use #:cl
+        #:fiveam))
+(in-package :screenshotbot/dashboard/test-batch)
+
+
+(util/fiveam:def-suite)
