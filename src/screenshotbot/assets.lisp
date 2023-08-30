42ea29f260a854fd26e042f221a410d335ce0f5a
Arnold Noronha <arnold@tdrhq.com>
2023-08-30 14:26:32 -0400

    Revert "Add a retry when fetching the recorder"
    This reverts commit b0a5637a7f6b20ac1ace8e694f16c717db154b02.
    
    GitOrigin-RevId: 667c6d68fc1e1735bd301172383637178f7d9525

--- src/screenshotbot/assets.lisp
+++ src/screenshotbot/assets.lisp
@@ -99,12 +99,11 @@ set -x
 type=`uname`
 
 INSTALLER=screenshotbot-installer.sh
-CURL=\"curl --retry 3 --retry-all-errors\"
 
 if [ $type = \"Linux\" ] ; then
-  $CURL --progress-bar ${linux-link} --output $INSTALLER
+  curl --progress-bar ${linux-link} --output $INSTALLER
 elif [ $type = \"Darwin\" ] ; then
-  $CURL --progress-bar  ${darwin-link} --output $INSTALLER
+  curl --progress-bar  ${darwin-link} --output $INSTALLER
 else
   echo Unknown uname type: $type, please message support@screenshotbot.io
 fi
