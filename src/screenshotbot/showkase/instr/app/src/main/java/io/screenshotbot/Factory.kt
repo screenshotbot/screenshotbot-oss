492dccaeda0f4c818d29777522e9765bb7ce7cff
Arnold Noronha <arnold@tdrhq.com>
2023-10-24 00:56:44 +0100

    Add the kotlin code that makes everything build
    Differential Revision: https://phabricator.tdrhq.com/D8117
    
    GitOrigin-RevId: 03a8093479bc645be9e2067bf5a36733e4ac7887

--- src/screenshotbot/showkase/instr/app/src/main/java/io/screenshotbot/Factory.kt
+++ src/screenshotbot/showkase/instr/app/src/main/java/io/screenshotbot/Factory.kt
@@ -1,9 +1,22 @@
 package io.screenshotbot
 
+import android.graphics.Bitmap
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule.*
+import org.junit.runners.model.Statement
+import sergio.sastre.uitesting.utils.activityscenario.ActivityScenarioForComposableRule
+import sergio.sastre.uitesting.utils.activityscenario.ComposableConfigItem
+import sergio.sastre.uitesting.utils.common.DisplaySize
+import sergio.sastre.uitesting.utils.common.FontSize
+import sergio.sastre.uitesting.utils.common.Orientation
+import sergio.sastre.uitesting.utils.common.UiMode
+import androidx.activity.compose.setContent
+import com.facebook.testing.screenshot.Screenshot
+import org.junit.runner.Description
+
 
 fun createTestRule() : ComposeTestRule {
     return createComposeRule()
@@ -12,3 +25,28 @@ fun createTestRule() : ComposeTestRule {
 fun createEmptyTestRule() : ComposeTestRule {
     return createEmptyComposeRule()
 }
+
+
+fun composableToBitmap(composable: @Composable () -> Unit): Bitmap? {
+    val rule: ActivityScenarioForComposableRule = ActivityScenarioForComposableRule(
+            config = ComposableConfigItem(
+                    locale = "en",
+                    uiMode = UiMode.DAY,
+                    orientation = Orientation.PORTRAIT,
+                    fontSize = FontSize.NORMAL,
+                    displaySize = DisplaySize.NORMAL
+            )
+    )
+
+    val stmt = object : Statement() {
+        override fun evaluate() {
+            rule.composeView.setContent(composable)
+            Screenshot.snap(rule.composeView)
+                    .record()
+        }
+    }
+
+    rule.apply(stmt, Description.EMPTY);
+    return null
+}
+
