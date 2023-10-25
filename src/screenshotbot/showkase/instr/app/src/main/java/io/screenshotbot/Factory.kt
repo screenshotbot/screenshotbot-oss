fa18df54cae062c23854c752e00d8b0ca36c5ff1
Arnold Noronha <arnold@tdrhq.com>
2023-10-25 08:12:59 -0400

    Undo some of the last few changes
    Summary:
    The pulled in dependencies are conflicting, and I might have
    to do this with reflection instead.
    
    Differential Revision: https://phabricator.tdrhq.com/D8120
    
    GitOrigin-RevId: 22d8de8482fcf6e51baa7c6617936ef6ce4a198b

--- src/screenshotbot/showkase/instr/app/src/main/java/io/screenshotbot/Factory.kt
+++ src/screenshotbot/showkase/instr/app/src/main/java/io/screenshotbot/Factory.kt
@@ -7,17 +7,10 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule.*
 import org.junit.runners.model.Statement
-import sergio.sastre.uitesting.utils.activityscenario.ActivityScenarioForComposableRule
-import sergio.sastre.uitesting.utils.activityscenario.ComposableConfigItem
-import sergio.sastre.uitesting.utils.common.DisplaySize
-import sergio.sastre.uitesting.utils.common.FontSize
-import sergio.sastre.uitesting.utils.common.Orientation
-import sergio.sastre.uitesting.utils.common.UiMode
 import androidx.activity.compose.setContent
 import com.facebook.testing.screenshot.Screenshot
 import org.junit.runner.Description
 import android.app.Activity
-import androidx.test.core.app.ActivityScenario
 import android.view.*
 import android.graphics.*
 
@@ -28,43 +21,4 @@ fun createTestRule() : ComposeTestRule {
 
 fun createEmptyTestRule() : ComposeTestRule {
     return createEmptyComposeRule()
-}
-
-fun ActivityScenarioForComposableRule.setContent(
-    content: @Composable () -> Unit
-): ActivityScenario<out Activity> =
-    activityScenario.onActivity {
-        it.setContent { content() }
-    }
-
-fun viewToBitmap(view: View): Bitmap? {
-    val bmp = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
-                                  Bitmap.Config.ARGB_8888);
-    val canvas = Canvas(bmp);
-    view.draw(canvas);
-    return bmp;
-}
-
-fun composableToBitmap(composable: @Composable () -> Unit): Bitmap? {
-    val rule: ActivityScenarioForComposableRule = ActivityScenarioForComposableRule(
-            config = ComposableConfigItem(
-                    locale = "en",
-                    uiMode = UiMode.DAY,
-                    orientation = Orientation.PORTRAIT,
-                    fontSize = FontSize.NORMAL,
-                    displaySize = DisplaySize.NORMAL
-            )
-    )
-
-    var bmp: Bitmap? = null
-
-    val stmt = object : Statement() {
-        override fun evaluate() {
-            rule.setContent(composable);
-            bmp = viewToBitmap(rule.composeView);
-        }
-    }
-
-    rule.apply(stmt, Description.EMPTY);
-    return bmp;
 }
