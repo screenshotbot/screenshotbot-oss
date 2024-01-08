af2a9dabb9c80745996a1fff694234bfb43b1d65
Arnold Noronha <arnold@tdrhq.com>
2024-01-08 15:05:07 -0500

    Rename to util/events
    Differential Revision: https://phabricator.tdrhq.com/D8531
    
    GitOrigin-RevId: 4f410ba3661b1db8e013c209d2d6d7a61e189fdf

--- src/util/events.lisp
+++ src/util/events.lisp
@@ -4,9 +4,9 @@
 ;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(defpackage :screenshotbot/events
+(defpackage :util/events
   (:use #:cl)
-  (:nicknames :util/events)
+  (:nicknames :screenshotbot/events)
   (:import-from #:easy-macros
                 #:def-easy-macro)
   (:import-from #:clsql-sys
