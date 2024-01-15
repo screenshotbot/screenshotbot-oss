screenshotbot/screenshotbot-oss

commit 59d1d64c96a853c3380b96cfb2bbf4da0bb9c07a

Don't rely on company-invites to get the invites
Summary:
Removing this dependency lets us use the invite model for
non-screenshotbot uses.

Differential Revision: https://phabricator.tdrhq.com/D8607

GitOrigin-RevId: 9f16710a25e2e3115077cff2e4691d22622f49ea

src/screenshotbot/screenshotbot.asd
@@ -390,6 +390,7 @@
                              (:file "test-common")))
                (:module "model"
                 :components ((:file "test-core")
+                             (:file "test-invite")
                              (:file "test-auto-cleanup")
                              (:file "test-transient-object")
                              (:file "test-recorder-run")

src/screenshotbot/model/invite.lisp
@@ -9,9 +9,6 @@
   (:import-from #:bknr.datastore
                 #:persistent-class
                 #:store-object)
-  (:import-from #:screenshotbot/model/company
-                #:all-companies
-                #:company-invites)
   (:import-from #:screenshotbot/notice-api
                 #:invite-company)
   (:import-from #:util
@@ -54,16 +51,14 @@
    (format out "#<INVITE ~a>" email)))
 
 (defmethod initialize-instance :after ((invite invite) &key company &allow-other-keys)
-  (assert company)
-  (push invite (company-invites company)))
+  (assert company))
 
 (defun all-invites (&key company)
-  (cond
-    (company
-     (company-invites company))
-    (t
-     (loop for company in (all-companies)
-           appending (company-invites company)))))
+  (loop for invite in (bknr.datastore:class-instances 'invite)
+        if (or
+            (not company)
+            (eql company (invite-company invite)))
+          collect invite))
 
 (defun invites-with-email (email &key company)
   (loop for invite in (all-invites :company company)

src/screenshotbot/model/test-invite.lisp
@@ -1,0 +1,54 @@
+;;;; Copyright 2018-Present Modern Interpreters Inc.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+(defpackage :screenshotbot/model/test-invite
+  (:use #:cl
+        #:fiveam)
+  (:import-from #:screenshotbot/model/invite
+                #:invite-code
+                #:invite-with-code
+                #:invites-with-email
+                #:all-invites
+                #:invite)
+  (:import-from #:util/store/store
+                #:with-test-store)
+  (:import-from #:screenshotbot/model/company
+                #:company)
+  (:import-from #:fiveam-matchers/core
+                #:is-equal-to
+                #:assert-that)
+  (:import-from #:fiveam-matchers/lists
+                #:contains))
+(in-package :screenshotbot/model/test-invite)
+
+(util/fiveam:def-suite)
+
+(def-fixture state ()
+  (with-test-store ()
+    (let ((company (make-instance 'company))
+          (company-2 (make-instance 'company)))
+      (let ((invite (make-instance 'invite :company company
+                                           :email "foo@example.com"))
+          (invite2 (make-instance 'invite :company company-2
+                                          :email "bar@example.com")))
+       (&body)))))
+
+(test all-invites ()
+  (with-fixture state ()
+    (assert-that (all-invites :company company)
+                 (contains invite))
+    (assert-that (all-invites)
+                 (contains invite invite2))))
+
+(test invites-with-email ()
+  (with-fixture state ()
+    (assert-that (invites-with-email "foo@example.com")
+                 (contains invite))))
+
+(test invite-with-code ()
+  (with-fixture state ()
+    (assert-that (invite-with-code (invite-code invite))
+                 (is-equal-to invite))))

src/screenshotbot/company/members.lisp
@@ -19,13 +19,13 @@
   (:import-from #:screenshotbot/model/company
                 #:company-admin-p
                 #:company-admins
-                #:company-owner
-                #:company-invites)
+                #:company-owner)
   (:import-from #:screenshotbot/model/user
                 #:users-for-company)
   (:import-from #:nibble
                 #:nibble)
   (:import-from #:screenshotbot/model/invite
+                #:all-invites
                 #:invite-email)
   (:import-from #:bknr.datastore
                 #:with-transaction)
@@ -100,8 +100,7 @@
   (confirmation-page
    :yes (nibble ()
           (with-transaction ()
-            (a:deletef (company-invites company)
-                     invite))
+            (bknr.datastore:delete-object invite))
           (hex:safe-redirect "/settings/members"))
    :no "/settings/members"
    <span>Delete invitation to ,(invite-email invite)?</span>))
@@ -132,7 +131,7 @@
 
        ,(taskie-list
          :headers '("Name" "Email" "Status" "Actions")
-         :items (company-invites company)
+         :items (all-invites :company company)
          :checkboxes nil
          :empty-message "No pending invites"
          :row-generator (lambda (invite)

src/screenshotbot/model/company.lisp
@@ -41,7 +41,6 @@
    #:company-reports
    #:github-config
    #:access-token
-   #:company-invites
    #:company-channels
    #:jira-config-url
    #:jira-config-username
@@ -109,7 +108,7 @@
       :accessor company-reports)
      (invites
       :initform nil
-      :accessor company-invites)
+      :documentation "DEPRECATED")
      (channels
       :initarg :channels
       :initform nil

commit 926aea7849450ae4384c42d24e9ec7f210c3a291

Fix a missing function in ACCEPT-INVITE.
Summary:
Regressed in D8607

Fixes T1000. (Woo, task number 1000!)

Maniphest Tasks: T1000

Differential Revision: https://phabricator.tdrhq.com/D8628

GitOrigin-RevId: 7b3396f2b016025ff0552879844d62bb592b5e90

src/screenshotbot/invite.lisp
@@ -31,8 +31,7 @@
                 #:with-transaction
                 #:store-object-with-id)
   (:import-from #:screenshotbot/model/company
-                #:company-name
-                #:company-invites)
+                #:company-name)
   (:import-from #:screenshotbot/user-api
                 #:current-company
                 #:current-user)
@@ -213,18 +212,18 @@
     (incf (email-count invite))))
 
 (defun accept-invite (invite)
-  (when (member invite (unaccepted-invites (current-user)))
+  (%accept-invite invite (current-user))
+  (Setf (current-company) (invite-company invite))
+  (redirect-home))
+
+(defun %accept-invite (invite user)
+  (when (member invite (unaccepted-invites user))
     ;; Let's do the accept
     (with-transaction ()
-      (deletef (unaccepted-invites (current-user)) invite)
+      (deletef (unaccepted-invites user) invite)
       (pushnew
        (invite-company invite)
-       (user-companies (current-user)))
-      (deletef
-       (company-invites (invite-company invite))
-       invite))
-    (Setf (current-company) (invite-company invite)))
-  (redirect-home))
+       (user-companies user)))))
 
 (defhandler (invite-accept  :uri  "/invite/accept") (invite-id)
   (let ((invite (store-object-with-id (parse-integer invite-id))))

src/screenshotbot/screenshotbot.asd
@@ -390,6 +390,7 @@
                              (:file "test-login")
                              (:file "test-signup")
                              (:file "test-common")))
+               (:file "test-invite")
                (:module "model"
                 :components ((:file "test-core")
                              (:file "test-invite")

src/screenshotbot/test-invite.lisp
@@ -1,0 +1,65 @@
+;;;; Copyright 2018-Present Modern Interpreters Inc.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+(defpackage :screenshotbot/test-invite
+  (:use #:cl
+        #:fiveam)
+  (:import-from #:screenshotbot/model/company
+                #:company)
+  (:import-from #:util/store/store
+                #:with-test-store)
+  (:import-from #:screenshotbot/model/user
+                #:users-for-company
+                #:make-user)
+  (:import-from #:screenshotbot/installation
+                #:installation
+                #:multi-org-feature)
+  (:import-from #:screenshotbot/testing
+                #:with-installation)
+  (:import-from #:screenshotbot/model/invite
+                #:invite)
+  (:import-from #:util/testing
+                #:with-fake-request)
+  (:import-from #:screenshotbot/invite
+                #:%accept-invite
+                #:accept-invite)
+  (:import-from #:fiveam-matchers/core
+                #:is-not
+                #:assert-that)
+  (:import-from #:fiveam-matchers/lists
+                #:has-item)
+  (:import-from #:screenshotbot/user-api
+                #:unaccepted-invites))
+(in-package :screenshotbot/test-invite)
+
+(util/fiveam:def-suite)
+
+(defclass my-installation (multi-org-feature
+                           installation)
+  ())
+
+(def-fixture state ()
+  (with-installation (:installation (make-instance 'my-installation))
+   (with-test-store ()
+     (let* ((company (make-instance 'company))
+            (user (make-user))
+            (other-user (make-user :email "bar@example.com")))
+       (&body)))))
+
+
+(test simple-accept-invite
+  (with-fixture state ()
+    (let ((invite (make-instance 'invite
+                                 :inviter user
+                                 :company company
+                                 :email "bar@example.com")))
+      (setf (unaccepted-invites other-user)
+            (list invite))
+      (assert-that (users-for-company company)
+                   (is-not (has-item other-user)))
+      (%accept-invite invite other-user)
+      (assert-that (users-for-company company)
+                   (has-item other-user)))))