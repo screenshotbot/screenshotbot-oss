e6990a1680de2c67cd79916f3d3f4fce65d7de3d
Arnold Noronha <arnold@tdrhq.com>
2024-03-25 14:31:44 -0400

    Add a happy path test for company graph
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D9184
    
    GitOrigin-RevId: b829d6b876e8a80f11d0eb15e980d5b3bc6acf19

--- src/screenshotbot/model/test-company-graph.lisp
+++ src/screenshotbot/model/test-company-graph.lisp
@@ -6,7 +6,34 @@
 
 (defpackage :screenshotbot/model/test-company-graph
   (:use #:cl
-        #:fiveam))
+        #:fiveam)
+  (:import-from #:util/store/store
+                #:with-test-store)
+  (:import-from #:screenshotbot/testing
+                #:multi-org-test-installation
+                #:with-installation
+                #:with-test-user)
+  (:import-from #:screenshotbot/model/company-graph
+                #:company-graph)
+  (:import-from #:fiveam-matchers/lists
+                #:has-item)
+  (:import-from #:fiveam-matchers/core
+                #:is-not
+                #:assert-that))
 (in-package :screenshotbot/model/test-company-graph)
 
 (util/fiveam:def-suite)
+
+(def-fixture state ()
+  (with-test-store ()
+    (&body)))
+
+(test happy-path-graph
+  (with-fixture state ()
+    (with-installation (:installation (make-instance 'multi-org-test-installation))
+     (with-test-user (:user user :company company)
+       (assert-that (company-graph company)
+                    (has-item user))
+       (with-test-user (:user other-user :company company2 :company-name "other company")
+         (assert-that (company-graph company)
+                      (is-not (has-item other-user))))))))
