caf9759fe87f3d3dcf1aeb1b2cbe21a76e721883
Arnold Noronha <arnold@tdrhq.com>
2024-08-31 12:24:03 -0400

    Initial scaffolding for analytics dashboard
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D9925
    
    GitOrigin-RevId: 9f5e3564c7c741e4a1fa160cab41d9dc53d4694d

---
+++ src/screenshotbot/analytics-dashboard/dashboard.lisp
@@ -0,0 +1,48 @@
+;;;; Copyright 2018-Present Modern Interpreters Inc.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+(defpackage :screenshotbot/analytics-dashboard/dashboard
+  (:use #:cl)
+  (:import-from #:screenshotbot/server
+                #:defhandler)
+  (:import-from #:screenshotbot/template
+                #:app-template))
+(in-package :screenshotbot/analytics-dashboard/dashboard)
+
+(named-readtables:in-readtable markup:syntax)
+
+(defhandler (nil :uri "/analytics") ()
+  <app-template>
+
+    <div>
+      <canvas id="myChart"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+  </app-template>)
