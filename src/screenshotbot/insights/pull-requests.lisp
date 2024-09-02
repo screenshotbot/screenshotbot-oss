3eccd29d67593769bbb46cfc6b2f94816292fbae
Arnold Noronha <arnold@tdrhq.com>
2024-09-02 17:02:56 -0400

    optimize reports-for-run
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D9976
    
    GitOrigin-RevId: 344ddd767dad7216e151518ae7d1b00b807ed06d

--- src/screenshotbot/insights/pull-requests.lisp
+++ src/screenshotbot/insights/pull-requests.lisp
@@ -31,6 +31,30 @@
    ;; This is incorrect, needs revision here:
    (phabricator-diff-id run)))
 
+(defvar *reports-for-run* (make-hash-table :test #'eql))
+
+(defun fast-reports-for-run (run)
+  (cond
+    ((> (screenshotbot/model/recorder-run::%created-at run)
+        (- (get-universal-time)
+           3600))
+     (reports-for-run run))
+    (t
+     (cdr
+      (util:or-setf
+       (gethash run *reports-for-run*)
+       (list*
+        :dummy
+        (reports-for-run run)))))))
+
+(easy-macros:def-easy-macro do-run-report (&binding run &binding report
+                                                    company
+                                                    &key num-days
+                                                    &fn fn)
+  (loop for run in (runs-for-last-60-days company :num-days num-days) do
+    (loop for report in (fast-reports-for-run run) do
+      (fn run report))))
+
 (defun pr-to-actions (company)
   (let ((actions (make-hash-table :test #'equal))
         (runs (runs-for-last-60-days company :num-days 30)))
@@ -38,34 +62,31 @@
           if (safe-pr run)
           do (setf (gethash (safe-pr run) actions)
                    :none))
-    (loop for run in runs do
-      (loop for report in (reports-for-run run) do
-        (when (eql :none (gethash (safe-pr run) actions))
-          (setf (gethash (safe-pr run) actions)
-                :changed))
-        (when-let ((acceptable (report-acceptable report)))
-          (case (acceptable-state acceptable)
-            (:rejected
+    (do-run-report (run report company :num-days 30)
+      (when (eql :none (gethash (safe-pr run) actions))
+        (setf (gethash (safe-pr run) actions)
+              :changed))
+      (when-let ((acceptable (report-acceptable report)))
+        (case (acceptable-state acceptable)
+          (:rejected
+           (setf (gethash (safe-pr run) actions)
+                 :rejected))
+          (:accepted
+           (when (eql :changed #| should not be :none |#
+                      (gethash (safe-pr run) actions))
              (setf (gethash (safe-pr run) actions)
-                   :rejected))
-            (:accepted
-             (when (eql :changed #| should not be :none |#
-                        (gethash (safe-pr run) actions))
-               (setf (gethash (safe-pr run) actions)
-                     :accepted)))))))
+                   :accepted))))))
     actions))
 
 (defun user-reviews-last-30-days (company)
-  (let ((runs (runs-for-last-60-days company :num-days 30))
-        (result (make-hash-table)))
-    (dolist (run runs)
-      (dolist (report (reports-for-run run))
-        (when-let ((acceptable (report-acceptable report)))
-          (dolist (user
-                   (remove-duplicates
-                    (loop for history-item in (acceptable-history acceptable)
-                          collect (acceptable-history-item-user history-item))))
-            (incf (gethash user result 0))))))
+  (let ((result (make-hash-table)))
+    (do-run-report (run report company :num-days 30)
+      (when-let ((acceptable (report-acceptable report)))
+        (dolist (user
+                 (remove-duplicates
+                  (loop for history-item in (acceptable-history acceptable)
+                        collect (acceptable-history-item-user history-item))))
+          (incf (gethash user result 0)))))
     (sort
      (loop for user being the hash-keys of result
              using (hash-value count)
