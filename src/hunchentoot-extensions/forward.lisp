14e582a56071f8b7d675d0f772f14a5c122da9a1
Arnold Noronha <arnold@tdrhq.com>
2024-09-26 11:41:19 -0400

    Support the real-host mechanism for forward-request
    Summary:
    I'm working on T1380. Not sure if this is the way I'm going
    to unblock myself yet.
    
    Differential Revision: https://phabricator.tdrhq.com/D10083
    
    GitOrigin-RevId: 6d962c228271a5a29df069c6acf3aa478b87a947

--- src/hunchentoot-extensions/forward.lisp
+++ src/hunchentoot-extensions/forward.lisp
@@ -15,54 +15,61 @@
 
 
 (auto-restart:with-auto-restart ()
- (defun forward-request (url &optional (request hunchentoot:*request*))
-   ;; We'll avoid using util/http-request for now. It adds a depedency,
-   ;; but probably we want a lot more control over the behavior.
-   (let ((body (hunchentoot:raw-post-data :force-binary t))
-         (dest-uri (quri:render-uri
-                    (let ((uri (quri:uri url)))
+  (defun forward-request (url &key (request hunchentoot:*request*)
+                                (keep-current-host nil))
+    "Forward the current request to the destination URL. If
+keep-current-host is T, then we'll forward to the destination, but
+keep the Host header the same."
+    ;; We'll avoid using util/http-request for now. It adds a depedency,
+    ;; but probably we want a lot more control over the behavior.
+    (let* ((body (hunchentoot:raw-post-data :force-binary t))
+           (uri (quri:uri url))
+           (dest-uri (quri:render-uri
                       (quri:make-uri
                        :scheme (quri:uri-scheme uri)
-                       :host (quri:uri-host uri)
+                       :host (if keep-current-host
+                                 (hunchentoot:host request)
+                                 (quri:uri-host uri))
                        :port (quri:uri-port uri)
-                       :defaults (quri:uri (hunchentoot:request-uri request)))))))
-     (multiple-value-bind (response code headers)
-         (drakma:http-request
-          dest-uri
-          :additional-headers (list*
-                               (cons :x-forwarded-host (hunchentoot:host request))
-                               (loop for (key . value) in (hunchentoot:headers-in request)
-                                     unless (member key '(:content-length :user-agent :host
-                                                          :|:AUTHORITY:|
-                                                          :|:METHOD:|
-                                                          :|:PATH:|
-                                                          :|:SCHEME:|
-                                                          :accept-encoding))
-                                       collect (cons key value)))
-          :method (hunchentoot:Request-method request)
-          :force-binary t
-          :want-stream t
-          :decode-content nil
-          :redirect nil
-          :user-agent (hunchentoot:header-in :User-agent request)
-          :content-type (hunchentoot:header-in :content-type request)
-          :content body)
-       (setf (hunchentoot:return-code*) code)
+                       :defaults (quri:uri (hunchentoot:request-uri request))))))
+      (multiple-value-bind (response code headers)
+          (drakma:http-request
+           dest-uri
+           :additional-headers (list*
+                                (cons :x-forwarded-host (hunchentoot:host request))
+                                (loop for (key . value) in (hunchentoot:headers-in request)
+                                      unless (member key '(:content-length :user-agent :host
+                                                           :|:AUTHORITY:|
+                                                           :|:METHOD:|
+                                                           :|:PATH:|
+                                                           :|:SCHEME:|
+                                                           :accept-encoding))
+                                        collect (cons key value)))
+           :method (hunchentoot:Request-method request)
+           :real-host (quri:uri-host uri)
+           :force-binary t
+           :want-stream t
+           :decode-content nil
+           :redirect nil
+           :user-agent (hunchentoot:header-in :User-agent request)
+           :content-type (hunchentoot:header-in :content-type request)
+           :content body)
+        (setf (hunchentoot:return-code*) code)
 
-       (loop for (key . value) in headers
-             if (not (member key '(:content-length) ))
-               do (setf (hunchentoot:header-out key) value))
+        (loop for (key . value) in headers
+              if (not (member key '(:content-length) ))
+                do (setf (hunchentoot:header-out key) value))
 
-       (when-let ((Content-length (assoc-value headers :content-length)))
-        (setf (hunchentoot:header-out :content-length)
-              (parse-integer content-length)))
+        (when-let ((Content-length (assoc-value headers :content-length)))
+          (setf (hunchentoot:header-out :content-length)
+                (parse-integer content-length)))
 
-       (setf (hunchentoot:header-out :x-final-code) code)
-       (Setf (hunchentoot:header-out :x-final-headers) (format nil "~a" headers))
-       (setf (hunchentoot:Header-out :x-dest-uri) (format nil "~a" dest-uri))
+        (setf (hunchentoot:header-out :x-final-code) code)
+        (Setf (hunchentoot:header-out :x-final-headers) (format nil "~a" headers))
+        (setf (hunchentoot:Header-out :x-dest-uri) (format nil "~a" dest-uri))
 
-       (let* ((stream (hunchentoot:send-headers)))
-         (uiop:copy-stream-to-stream response stream
-                                     :element-type 'flex:octet)
-         (finish-output stream)
-         (hunchentoot:abort-request-handler))))))
+        (let* ((stream (hunchentoot:send-headers)))
+          (uiop:copy-stream-to-stream response stream
+                                      :element-type 'flex:octet)
+          (finish-output stream)
+          (hunchentoot:abort-request-handler))))))
