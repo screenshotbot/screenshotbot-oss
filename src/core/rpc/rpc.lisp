b623eb9142068c57fd043fa2e3690104324f7519
Arnold Noronha <arnold@tdrhq.com>
2024-10-23 10:42:09 -0400

    Increase map-rpc timeout
    Summary:
    From preset, it looks like sync-sha-request is taking 340s or
    so (from the tracing that we added in D10094).
    
    Fixes T1385, T1399
    
    Maniphest Tasks: T1399, T1385
    
    Differential Revision: https://phabricator.tdrhq.com/D10165
    
    GitOrigin-RevId: 3f861d049522f957a3d9cd82f0e432a0d46be9b1

--- src/core/rpc/rpc.lisp
+++ src/core/rpc/rpc.lisp
@@ -88,7 +88,7 @@
   (log:info "hello world!!")
   "123")
 
-(defun send-rpc (url rpc &key (read-timeout 300))
+(defun send-rpc (url rpc &key (read-timeout 600))
   (decode-bknr-object
    (http-request
     url
@@ -106,7 +106,7 @@
 (defun %port ()
   (hunchentoot:acceptor-port server::*multi-acceptor*))
 
-(defun map-rpc (rpc &key (read-timeout 300))
+(defun map-rpc (rpc &key (read-timeout 600))
   "Map the RPC across all the peers in the cluster (including the current-one)"
   (let* ((servers (%servers))
          (results (make-array (length servers))))
