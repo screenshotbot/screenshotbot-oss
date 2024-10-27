ddec5520814009524cb0581a6ae8d601b1fd02e8
Arnold Noronha <arnold@tdrhq.com>
2024-10-27 14:25:20 -0400

    Add a health-check for making sure HEIC and JXL is available
    Summary: Fixes T1431
    
    Maniphest Tasks: T1431
    
    Differential Revision: https://phabricator.tdrhq.com/D10173
    
    GitOrigin-RevId: f3aae1b3ac79ec30c7377a7f59ea3d86bed4cfb7

--- src/screenshotbot/magick/health-checks.lisp
+++ src/screenshotbot/magick/health-checks.lisp
@@ -8,7 +8,11 @@
   (:use #:cl)
   (:import-from #:util/health-check
                 #:def-health-check)
+  (:import-from #:screenshotbot/magick/magick
+                #:*magick*)
   (:import-from #:screenshotbot/magick/magick-lw
+                #:ping-image-metadata
+                #:save-wand-to-file
                 #:map-non-alpha-pixels
                 #:magick-write-image
                 #:with-wand)
@@ -26,10 +30,13 @@
 (defvar *rose* (read-file-sequence (asdf:system-relative-pathname
                                     :screenshotbot "fixture/rose.webp")))
 
-(def-easy-macro with-rose-wand (&binding wand &fn fn)
+(defvar *heic* (read-file-sequence (asdf:system-relative-pathname
+                                    :screenshotbot "fixture/rose.heic")))
+
+(def-easy-macro with-rose-wand (&binding wand &key (content *rose*) &fn fn)
   (uiop:with-temporary-file (:stream webp-s :pathname webp :type "webp"
                               :element-type '(unsigned-byte 8))
-    (write-sequence *rose* webp-s)
+    (write-sequence content webp-s)
     (finish-output webp-s)
     (with-wand (wand :file webp)
       (fn wand))))
@@ -40,7 +47,44 @@
                           (dbg:output-backtrace :brief))))
     (with-rose-wand (wand)
       (uiop:with-temporary-file (:pathname png :type "png")
-        (magick-write-image wand (namestring png))))))
+        (save-wand-to-file wand (namestring png))
+        (assert-equal "PNG" (%image-format png))))))
+
+(def-health-check load-heic-and-save-png ()
+  (handler-bind ((error (lambda (e)
+                          #+lispworks
+                          (dbg:output-backtrace :brief))))
+    (with-rose-wand (wand :content *heic*)
+      (uiop:with-temporary-file (:pathname png :type "png")
+        (save-wand-to-file wand (namestring png))
+        (assert-equal "PNG" (%image-format png))))))
+
+(defun %image-format (file)
+  (third (ping-image-metadata *magick* file)))
+
+(defun assert-equal (one two)
+  (unless (equal one two)
+    (error "failed: ~s is not equal to ~s"
+           one two)))
+
+(def-health-check load-webp-and-save-heic ()
+  (handler-bind ((error (lambda (e)
+                          #+lispworks
+                          (dbg:output-backtrace :brief))))
+    (with-rose-wand (wand)
+      (uiop:with-temporary-file (:pathname png :type "heic")
+        (save-wand-to-file wand (namestring png))
+        (assert-equal "HEIC" (%image-format png))))))
+
+(def-health-check load-webp-and-save-jxl ()
+  (handler-bind ((error (lambda (e)
+                          (format t "error: ~a~%" e)
+                          #+lispworks
+                          (dbg:output-backtrace :brief))))
+    (with-rose-wand (wand)
+      (uiop:with-temporary-file (:pathname png :type "jxl")
+        (save-wand-to-file wand (namestring png))
+        (assert-equal "JXL" (%image-format png))))))
 
 (def-health-check map-non-alpha-pixels ()
   (with-rose-wand (wand)
