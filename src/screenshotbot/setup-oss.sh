8a4a8363f74a629ae7d093ae46f0a378440a444d
Arnold Noronha <arnold@tdrhq.com>
2024-11-04 15:39:06 -0500

    Another import
    GitOrigin-RevId: e9431ff8f196f0494b4528562997d54979759d3e

--- src/screenshotbot/setup-oss.sh
+++ src/screenshotbot/setup-oss.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get update && apt-get install -y libyaml-dev git-core libpng-dev zlib1g-dev libpng16-16  zlib1g gcc makeself exiftool build-essential logrotate sbcl nginx certbot python3-certbot-nginx
+apt-get update && apt-get install -y libyaml-dev git-core libpng-dev zlib1g-dev libpng16-16  zlib1g gcc makeself exiftool build-essential logrotate sbcl nginx certbot python3-certbot-nginx pkg-config
 
 wget https://screenshotbot-assets.s3.us-east-1.amazonaws.com/imagemagick7_7.1.1-39_amd64.deb
 apt-get install -y ./imagemagick7_7.1.1-39_amd64.deb
