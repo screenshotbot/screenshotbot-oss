728fdfa91c335fd2818b9dc6356d614de40fb8e6
Arnold Noronha <arnold@tdrhq.com>
2024-11-22 13:37:16 -0500

    map for [date,channel,runs]
    Summary: T1529
    
    Differential Revision: https://phabricator.tdrhq.com/D10269
    
    GitOrigin-RevId: 3a2ca9d75e7918f8419724ec61bcbd5d08369b1d

--- src/screenshotbot/insights/maps.lisp
+++ src/screenshotbot/insights/maps.lisp
@@ -9,16 +9,34 @@
   (:import-from #:screenshotbot/insights/date
                 #:format-date)
   (:import-from #:screenshotbot/user-api
+                #:recorder-run-channel
                 #:%created-at))
 (in-package :screenshotbot/insights/maps)
+
+(defun %map-to-list (map)
+  (loop for key being the hash-keys of map
+            using (hash-value val)
+          collect (list key val)))
 
 (defun date-map (runs)
   "From runs create a map of (<date>, runs)"
   (let ((map (make-hash-table :test #'equal)))
     (dolist (run runs)
       (pushnew run (gethash (format-date (%created-at run)) map)))
-    (loop for key being the hash-keys of map
-            using (hash-value val)
-          collect (list key val))))
+    (%map-to-list map)))
+
+(defun channel-map (runs)
+  "From a list of runs, generate <channel>,runs"
+  (let ((map (make-hash-table)))
+    (dolist (run runs)
+      (pushnew run (gethash (recorder-run-channel run) map)))
+    (%map-to-list map)))
+
+(defun date-channel-map (runs)
+  (loop for (date date-runs) in (date-map runs)
+        for channel-map = (channel-map date-runs)
+        appending
+        (loop for item in channel-map
+              collect (list* date item))))
 
 
