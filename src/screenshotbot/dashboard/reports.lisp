4ac502f04dbbb112d2dfd41db5c180bbb575b825
Arnold Noronha <arnold@screenshotbot.io>
2024-12-11 21:46:16 -0500

    Explain this better
    Differential Revision: https://phabricator.tdrhq.com/D10416
    
    GitOrigin-RevId: f1e52c495f1b65634b98ad52400f9ae7b740a2ed

--- src/screenshotbot/dashboard/reports.lisp
+++ src/screenshotbot/dashboard/reports.lisp
@@ -451,7 +451,8 @@
 
                                      <explain>
                                        We use the Root-Mean-Squared-Error to compute this difference. This might not
-                                       correspond to whether or not this is a regression.
+                                       correspond to whether or not this is a regression. Many real regressions
+                                       small RMSE differences.
                                      </explain>
 
                                    </h4>
