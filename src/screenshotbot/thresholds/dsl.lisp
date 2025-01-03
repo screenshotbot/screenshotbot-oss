a2eb329a30ec6622d7717e98d84601cbf70de8b0
Arnold Noronha <arnold@screenshotbot.io>
2025-01-03 03:09:01 -0500

    Custom parser instead of lisp parser
    Differential Revision: https://phabricator.tdrhq.com/D10489
    
    GitOrigin-RevId: 35715c063a313dae6585d42c5f4ac5107e0ec5ea

--- src/screenshotbot/thresholds/dsl.lisp
+++ src/screenshotbot/thresholds/dsl.lisp
@@ -13,10 +13,46 @@
 (defun %read-dsl-from-string (code)
   (let ((input (make-string-input-stream code)))
     (let ((*package* (symbol-package 'foo)))
-     (%read-dsl input))))
+      (%read-dsl input))))
+
+(defun read-list (stream &optional read-so-far)
+  (let ((next (peek-char nil stream)))
+    (cond
+      ((eql #\) next)
+       (read-char stream)
+       (reverse read-so-far))
+      (t
+       (read-list
+        stream
+        (list* (%read-dsl stream) read-so-far))))))
+
+(defun read-symbol-as-string (stream)
+  (coerce
+   (loop for next = (peek-char nil stream)
+         until (or
+                (eql next #\)))
+         collect (read-char stream))
+   'string))
+
+(defparameter *symbols*
+  '(exact-images))
+
+(defun parse-symbol (string)
+  (loop for sym in *symbols*
+        if (equal (string-downcase sym) string)
+          return sym
+        finally
+         (error "Invalid symbol: ~a" string)))
 
 (defun %read-dsl (stream)
-  (read stream))
+  (let ((next (peek-char nil stream)))
+    (cond
+      ((eql #\( next)
+       (read-char stream)
+       (read-list stream))
+      (t
+       (parse-symbol
+        (read-symbol-as-string stream))))))
 
 
 
