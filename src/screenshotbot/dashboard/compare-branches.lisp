18f4e74716a226aae3a2623a0e59329460ca5327
Arnold Noronha <arnold@screenshotbot.io>
2025-02-02 12:40:26 -0500

    Form for comparing the two branches
    Summary: But the actual comparison is not implemented
    
    Differential Revision: https://phabricator.tdrhq.com/D10572
    
    GitOrigin-RevId: 5733e0d984a8ab2143fb790e60532799cd44eed9

--- src/screenshotbot/dashboard/compare-branches.lisp
+++ src/screenshotbot/dashboard/compare-branches.lisp
@@ -9,15 +9,44 @@
   (:import-from #:screenshotbot/server
                 #:defhandler)
   (:import-from #:screenshotbot/template
-                #:app-template))
+                #:app-template)
+  (:import-from #:core/ui/simple-card-page
+                #:simple-card-page)
+  (:import-from #:nibble
+                #:nibble))
 (in-package :screenshotbot/dashboard/compare-branches)
 
 (named-readtables:in-readtable markup:syntax)
 
 (defhandler (nil :uri "/compare-branches") ()
   (assert (gk:check :compare-branches (auth:current-company)))
-  <app-template>
-    hello world
-  </app-template>)
+  (let ((action (nibble (sha1 sha2)
+                  (declare (ignore sha1 sha2))
+                  (error "unimpl"))))
+    <simple-card-page form-action=action >
+      <div class= "card-header">
+        <h4>Compare branches or commits</h4>
+      </div>
+      <div class= "card-body">
+        <div class= "mb-2">
+          <label for= "sha1" class= "form-label" >
+            First SHA/branch
+          </label>
+          <input type= "text" class= "form-control" id= "sha1" name= "sha1" placeholder= "abcdef0102" />
+        </div>
+
+        <div class= "mb-2">
+          <label for= "sha2" class= "form-label" >
+            Second SHA/branch
+          </label>
+          <input type= "text" class= "form-control" id= "sha2" name= "sha2" placeholder= "abcdef0102"/>
+        </div>
+
+      </div>
+      <div class= "card-footer">
+        <input type= "submit" value= "Compare" class= "btn btn-primary" />
+        <a href= "/" class= "btn btn-secondary" >Cancel</a>
+      </div>
+    </simple-card-page>))
 
 
