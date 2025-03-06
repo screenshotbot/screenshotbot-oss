069a66c245b7d447797f7e93d908f0573b89250c
Arnold Noronha <arnold@screenshotbot.io>
2025-03-06 13:22:51 -0500

    API call to accept a report
    Summary: Fixes T1783
    
    Maniphest Tasks: T1783
    
    Differential Revision: https://phabricator.tdrhq.com/D10764
    
    GitOrigin-RevId: 1f0fd1043a998c168c4c7eb77effc6ac30636da0

--- src/screenshotbot/api/report.lisp
+++ src/screenshotbot/api/report.lisp
@@ -5,7 +5,28 @@
 ;;;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 (defpackage :screenshotbot/api/report
-  (:use #:cl))
+  (:use #:cl)
+  (:import-from #:screenshotbot/api/core
+                #:defapi)
+  (:import-from #:screenshotbot/report-api
+                #:report-acceptable)
+  (:import-from #:alexandria
+                #:when-let)
+  (:import-from #:screenshotbot/model/report
+                #:report-to-dto
+                #:acceptable-state))
 (in-package :screenshotbot/api/report)
+
+(defapi (%report-accept :uri "/api/report/:oid/review/accept"
+                        :method :post
+                        :wrap-success t)
+        (oid)
+  (let ((report (util:find-by-oid oid)))
+    (auth:can-view! report)
+    (when-let ((acceptable (report-acceptable report)))
+      (setf (acceptable-state acceptable) :accepted))
+    (report-to-dto report)))
+
+
 
 
--- src/screenshotbot/api/test-report.lisp
+++ src/screenshotbot/api/test-report.lisp
@@ -6,9 +6,71 @@
 
 (defpackage :screenshotbot/api/test-report
   (:use #:cl
-        #:fiveam))
+        #:fiveam)
+  (:import-from #:screenshotbot/user-api
+                #:channel)
+  (:import-from #:screenshotbot/model/recorder-run
+                #:make-recorder-run)
+  (:import-from #:screenshotbot/testing
+                #:multi-org-test-installation
+                #:with-installation
+                #:with-test-user)
+  (:import-from #:util/store/store
+                #:with-test-store)
+  (:import-from #:screenshotbot/report-api
+                #:report)
+  (:import-from #:screenshotbot/api/report
+                #:%report-accept)
+  (:import-from #:util/store/object-id
+                #:oid)
+  (:import-from #:screenshotbot/model/report
+                #:acceptable-state
+                #:base-acceptable)
+  (:import-from #:bknr.datastore
+                #:persistent-class)
+  (:import-from #:fiveam-matchers/core
+                #:has-typep
+                #:assert-that)
+  (:local-nicknames (#:dto #:screenshotbot/api/model)))
 (in-package :screenshotbot/api/test-report)
 
-
 (util/fiveam:def-suite)
+
+(defclass fake-acceptable (base-acceptable)
+  ()
+  (:metaclass persistent-class))
+
+
+
+(def-fixture state ()
+  (with-installation (:installation (make-instance 'multi-org-test-installation))
+   (with-test-store ()
+     (with-test-user (:logged-in-p t
+                      :company company)
+       (let* ((channel (make-instance 'channel :company company))
+              (run (make-recorder-run  :channel channel
+                                       :company company
+                                       :screenshots nil))
+              (acceptable (make-instance 'fake-acceptable))
+              (report (make-instance 'report
+                                     :run run
+                                     :acceptable acceptable
+                                     :previous-run nil)))
+         (&body))))))
+
+(test simple-accept-test
+  (with-fixture state ()
+    (assert-that (%report-accept :oid (oid report))
+                 (has-typep 'dto:report))
+    (is (eql :accepted (acceptable-state acceptable)))))
+
+(test cannot-access-other-reports
+  (with-fixture state ()
+    (with-test-user (:logged-in-p t
+                     :company-name "other company"
+                     :company other-company)
+      (is (not (eql other-company company)))
+      (is (not (eql company (auth:current-company))))
+      (signals auth:no-access-error
+       (%report-accept :oid (oid report))))))
 
