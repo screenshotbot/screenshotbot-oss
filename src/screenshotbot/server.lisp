6d5fd727c1b01a09d749f78426e68222ef47e819
Arnold Noronha <arnold@screenshotbot.io>
2025-03-23 15:43:40 -0400

    More granular waiting for leader before sending to next machine
    Summary: Fixes T1802
    
    Maniphest Tasks: T1802
    
    Differential Revision: https://phabricator.tdrhq.com/D10834
    
    GitOrigin-RevId: 0e759c91c3bbb8c5da5682637bec8946c791a954

--- src/screenshotbot/server.lisp
+++ src/screenshotbot/server.lisp
@@ -31,6 +31,8 @@
                 #:acceptor-with-existing-socket)
   #+ (and lispworks linux)
   (:import-from #:bknr.cluster/server
+                #:wait-for-leader
+                #:leader-id
                 #:leaderp)
   (:import-from #:util/throttler
                 #:throttled-error)
@@ -221,15 +223,13 @@
     ;; If we're here, it's probably because the leader got transfered
     ;; recently. The new leader might not be ready yet, so this hack
     ;; slows down nginx before it retries the next backend.
-    (sleep 3)
-    (cond
-      ((leaderp bknr.datastore:*store*)
-       ;; If we've become the leader while we were waiting then just
-       ;; continue.
-       (values))
-      (t
-       (setf (hunchentoot:return-code*) 502)
-       (hunchentoot:abort-request-handler)))))
+    (unless (wait-for-leader bknr.datastore:*store* :timeout 15)
+      (warn "Responding to request as read-only"))
+
+    ;; At this point, we're either a leader or a leader wasn't elected
+    ;; in 15s. If we're not a leader, we'll effectively be responding
+    ;; read-only.
+    (values)))
 
 (defmethod hunchentoot:acceptor-dispatch-request ((acceptor acceptor) request)
   (maybe-redirect-to-leader request)
