287237f11b90c6d70f12340b62df590191955fa3
Arnold Noronha <arnold@screenshotbot.io>
2025-04-03 18:57:41 -0400

    Adding some more scaffolding...
    Summary: Still trying to figure out the best way to test this.
    
    Differential Revision: https://phabricator.tdrhq.com/D10876
    
    GitOrigin-RevId: ae2b083df159df375f680a431ea864f5f5e4c3c6

--- src/screenshotbot/sdk/dev/test-verify-against-ci.lisp
+++ src/screenshotbot/sdk/dev/test-verify-against-ci.lisp
@@ -6,14 +6,32 @@
 
 (defpackage :screenshotbot/sdk/dev/test-verify-against-ci
   (:use #:cl
-        #:fiveam))
+        #:fiveam)
+  (:import-from #:screenshotbot/sdk/integration-fixture
+                #:with-sdk-integration)
+  (:import-from #:screenshotbot/model/company
+                #:company)
+  (:import-from #:screenshotbot/user-api
+                #:channel)
+  (:import-from #:screenshotbot/model/recorder-run
+                #:make-recorder-run))
 (in-package :screenshotbot/sdk/dev/test-verify-against-ci)
 
 
 (util/fiveam:def-suite)
 
 (def-fixture state ()
-  (&body))
+  (with-sdk-integration (api-context)
+    (let* ((company (make-instance 'company))
+           (channel (make-instance 'channel
+                                   :company company
+                                   :name "foobar"))
+           (run (make-recorder-run
+                 :company company
+                 :commit-hash "abcd"
+                 :channel channel
+                 :screenshots nil)))
+      (&body))))
 
 (test verify-against-ci
   (with-fixture state ()
