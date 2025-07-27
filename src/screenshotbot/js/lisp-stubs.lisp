0a06507ab399235ab05c24a6c0fdab3429ca5549
Arnold Noronha <arnold@screenshotbot.io>
2025-07-27 17:30:53 -0400

    Add core-transform!
    Differential Revision: https://phabricator.tdrhq.com/D11280
    
    GitOrigin-RevId: 89468b168c6c777e52a7c12b95bd690862795d57

--- src/screenshotbot/js/lisp-stubs.lisp
+++ src/screenshotbot/js/lisp-stubs.lisp
@@ -1,7 +1,15 @@
 (in-package :screenshotbot-js-stubs)
 
-(defun make-matrix (a b c d e f)
+
+(defun make-matrix-impl (a b c d e f)
   (3d-matrices:mat
    a c e
    b d f
    0 0 1))
+
+(defvar *make-matrix-impl* #'make-matrix-impl)
+
+(defun make-matrix (&rest args)
+  (apply *make-matrix-impl* args))
+
+(make-matrix 1 1 1 1 1 1 )
