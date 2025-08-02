388d02a2a042f64e4b062311a317bb47a4ad0eb8
Arnold Noronha <arnold@screenshotbot.io>
2025-08-02 15:10:54 -0400

    Add a git-diff command stub
    Differential Revision: https://phabricator.tdrhq.com/D11360
    
    GitOrigin-RevId: 8b303a20867f251d03d3416a3a25736b8f6c68de

--- src/pixel-diff/main.lisp
+++ src/pixel-diff/main.lisp
@@ -18,34 +18,69 @@
 
 
 
+(defun git-diff-command ()
+  (clingon:make-command
+   :name "git-diff"
+   :description "Compare git revisions of images"
+   :usage "git-diff <image-path> [revision1] [revision2]"
+   :options (list)
+   :handler (lambda (cmd)
+              (declare (ignore cmd))
+              (error "unimplemented"))))
+
+(defun pixel-diff-command/handler (cmd)
+  (let ((args (clingon:command-arguments cmd)))
+    (when (< (length args) 2)
+      (format t "Usage: pixel-diff <image1> <image2>~%")
+      (format t "  Compare two images and display the differences~%")
+      (usage-error "Could not parse command line"))
+    
+    (let ((image1 (first args))
+          (image2 (second args)))
+      (unless (probe-file image1)
+        (usage-error "Error: Image file not found: ~a~%" image1))
+      
+      (unless (probe-file image2)
+        (usage-error "Error: Image file not found: ~a~%" image2))
+      
+      (format t "Comparing images:~%  Before: ~a~%  After:  ~a~%" image1 image2)
+      
+      (let ((interface (pixel-diff/differ:create-empty-interface
+                        :image1 (namestring image1)
+                        :image2 (namestring image2))))
+        (capi:display interface)
+        0))))
+
 (defun pixel-diff-command ()
   (clingon:make-command
    :name "pixel-diff"
    :description "Compare two images and display the differences"
-   :usage "pixel-diff <image1> <image2>"
+   :usage "pixel-diff <subcommand> [options] [args...]"
+   :options (list)
+   :sub-commands (list (git-diff-command)
+                       (help-command))
+   :handler #'pixel-diff-command/handler))
+
+(defun help-command ()
+  (clingon:make-command
+   :name "help"
+   :description "Show help information"
+   :usage "help [subcommand]"
    :options (list)
    :handler (lambda (cmd)
               (let ((args (clingon:command-arguments cmd)))
-                (when (< (length args) 2)
-                  (format t "Usage: pixel-diff <image1> <image2>~%")
-                  (format t "  Compare two images and display the differences~%")
-                  (usage-error "Could not parse command line"))
-                
-                (let ((image1 (first args))
-                      (image2 (second args)))
-                  (unless (probe-file image1)
-                    (usage-error "Error: Image file not found: ~a~%" image1))
-                  
-                  (unless (probe-file image2)
-                    (usage-error "Error: Image file not found: ~a~%" image2))
-                  
-                  (format t "Comparing images:~%  Before: ~a~%  After:  ~a~%" image1 image2)
-                  
-                  (let ((interface (pixel-diff/differ:create-empty-interface
-                                    :image1 (namestring image1)
-                                    :image2 (namestring image2))))
-                    (capi:display interface)
-                    0))))))
+                (if args
+                    (format t "Help for subcommand: ~a~%" (first args))
+                    (progn
+                      (format t "pixel-diff - Image comparison tool~%~%")
+                      (format t "Usage: pixel-diff <subcommand> [options] [args...]~%~%")
+                      (format t "Available subcommands:~%")
+                      (format t "  help      Show this help message~%")
+                      (format t "  git-diff  Compare git revisions of images~%~%")
+                      (format t "Direct usage: pixel-diff <image1> <image2>~%")
+                      (format t "  Compare two images directly~%")))
+                0))))
+
 
 (defun %main (args)
   "Main entry point for the pixel-diff application"
