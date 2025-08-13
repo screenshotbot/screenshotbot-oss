6b96d709c62c7784b437ba821c9ade2f7c93d303
Arnold Noronha <arnold@screenshotbot.io>
2025-08-13 11:56:12 -0400

    Cache the image in an LRU cache
    Summary:
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D11432
    
    GitOrigin-RevId: b8e0241ea0b26cdfc0c4c13eb63371b804f896f1

--- src/pixel-diff/external-images.lisp
+++ src/pixel-diff/external-images.lisp
@@ -7,8 +7,54 @@
    #:with-image-from-external))
 (in-package :pixel-diff/external-images)
 
+(defvar *clock* 0)
+
+(defclass cache-entry ()
+  ((pane :initarg :pane
+         :reader pane)
+   (external-image :initarg :external-image
+                   :reader external-image)
+   (image :initarg :image
+          :reader image)
+   (timestamp :initform *clock*
+              :accessor timestamp)))
+
+(defvar *cache* nil)
+(defvar *max-cache-size* 5)
+
+(defun maybe-evict-cache ()
+  (when (>= (length *cache*) *max-cache-size*)
+    (let ((oldest-entry (reduce (lambda (a b)
+                                  (if (< (timestamp a) (timestamp b))
+                                      a
+                                      b))
+                                *cache*)))
+      (gp:free-image
+       (pane oldest-entry)
+       (image oldest-entry))
+      (setf *cache* (remove oldest-entry *cache*)))))
+
+(defun find-cache-entry (pane external-image)
+  (find-if (lambda (entry)
+             (and (eq (pane entry) pane)
+                  (equal (external-image entry) external-image)))
+           *cache*))
+
+
 (def-easy-macro with-image-from-external (&binding image pane external-image &fn fn)
-  (let ((image (gp:load-image pane external-image :editable t)))
-    (unwind-protect
-         (fn image)
-      (gp:free-image pane image))))
+  (let ((existing (find-cache-entry pane external-image)))
+    (cond
+      (existing
+       (log:debug "using cached image")
+       (fn (image existing)))
+      (t
+       (log:debug "Loading fresh image")
+       (let ((image (gp:load-image pane external-image :editable t)))
+         (maybe-evict-cache)
+         (push
+          (make-instance 'cache-entry
+                         :pane pane
+                         :external-image external-image
+                         :image image)
+          *cache*)
+         (fn image))))))
