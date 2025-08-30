9ffe97789f4922b4287df32c7db6672ad1033308
Arnold Noronha <arnold@screenshotbot.io>
2025-08-30 10:35:22 -0400

    Fix logo to have margin, and be an exact square 607x607
    Summary: The XCF should be updated too
    
    Differential Revision: https://phabricator.tdrhq.com/D11552
    
    GitOrigin-RevId: cbad6ae279537677c4755a935237f80a5f8e9a91

--- src/screenshotbot/sdk/upload-commit-graph.lisp
+++ src/screenshotbot/sdk/upload-commit-graph.lisp
@@ -17,9 +17,16 @@
   (:import-from #:clingon.options
                 #:make-option)
   (:import-from #:screenshotbot/sdk/env
-                #:make-env-reader)
+                #:make-env-reader
+                #:build-url)
   (:import-from #:screenshotbot/sdk/commit-graph
                 #:commit-graph-updater)
+  (:import-from #:screenshotbot/sdk/request
+                #:request)
+  (:import-from #:screenshotbot/api/model
+                #:build-info)
+  (:import-from #:alexandria
+                #:when-let)
   (:local-nicknames (#:run-context #:screenshotbot/sdk/run-context)))
 (in-package :screenshotbot/sdk/upload-commit-graph)
 
@@ -49,6 +56,21 @@
               :key :main-branch))
    :description "Uploads the current commit graph. Useful if you need to upload multiple channels in the same CI job, and want to avoid uploading commit graph multiple times."))
 
+(defun push-build-info-if-xcode-cloud (api-context run-context)
+  "Push build-info if running in Xcode Cloud environment"
+  (when (uiop:getenv "CI_XCODE_CLOUD")
+    (when-let ((build-url (run-context:build-url run-context))
+               (repo-url (run-context:repo-url run-context)))
+      (log:info "Detected Xcode Cloud, pushing build-info with build-url: ~a and repo-url: ~a" 
+                build-url repo-url)
+      (let ((build-info-dto (make-instance 'build-info
+                                         :build-url build-url
+                                         :repo-url repo-url)))
+        (request api-context "/api/build-info"
+                 :method :put
+                 :content build-info-dto
+                 :decode-response nil)))))
+
 (defun upload-commit-graph/handler (cmd)
   (with-clingon-api-context (api-context cmd)
     (let ((run-context (make-instance 'commit-graph-run-context
@@ -57,6 +79,7 @@
       (unless (run-context:repo-url run-context)
         (error "Could not determine repository URL, please pass the --repo-url argument"))
       (log:info "Updating commit graph for ~a" (run-context:repo-url run-context))
+      (push-build-info-if-xcode-cloud api-context run-context)
       (update-commit-graph (make-instance 'commit-graph-updater :api-context api-context)
                            (run-context:git-repo run-context)
                            (run-context:main-branch run-context)))))
