f8033cd1563820fc7e4624bee07b4243942a5339
Arnold Noronha <arnold@screenshotbot.io>
2025-09-30 16:01:19 -0400

    Use the right title for documentation pages
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D11680
    
    GitOrigin-RevId: 827d05421606739d73b018cc9db91cb72762d08e

--- src/remark/all.lisp
+++ src/remark/all.lisp
@@ -6,7 +6,9 @@
   (:export #:defpage
            #:find-node
            #:defsection
+           #:locate-page
            #:deftoplevel
            #:render-table-of-contents
            #:render-page
+           #:section-title
            #:toplevel-prefix))
