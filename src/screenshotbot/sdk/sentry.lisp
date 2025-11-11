c3d95439a523ac02ebab4f43b0297c1bd529afdf
Arnold Noronha <arnold@screenshotbot.io>
2025-11-11 18:01:46 -0500

    Explicitly initialize Sentry when needed
    Differential Revision: https://phabricator.tdrhq.com/D11836
    
    GitOrigin-RevId: 47bacf6db6c12771e4e8b73382ac9360e074a444

--- src/screenshotbot/sdk/sentry.lisp
+++ src/screenshotbot/sdk/sentry.lisp
@@ -53,7 +53,8 @@
                                   &fn fn)
   #-screenshotbot-oss
   (sentry-client:initialize-sentry-client
-   sentry:*dsn* :client-class 'sentry:delivered-client)
+   "https://b0c00f1b8a0e499fbdba1de5013fc97b:0632f4ab8db64d55accd4b81c93e7b79@sentry.io/1460546"
+   :client-class 'sentry:delivered-client)
   (with-tags (("cli-client" "true")
               #+lispworks ("api_hostname" *hostname*)
               #+lispworks ("api_id"  *api-key*)
