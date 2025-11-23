069ca65aec03e525892152d002215632080f19e8
Arnold Noronha <arnold@screenshotbot.io>
2025-11-23 17:26:46 -0500

    [tidy] Fix indentation everywhere in skip-list.lisp
    Differential Revision: https://phabricator.tdrhq.com/D11925
    
    GitOrigin-RevId: be50cac5695caa4c8b9b4a14dfb42afaf51dfcd2

--- third-party/bknr.datastore/src/skip-list/skip-list.lisp
+++ third-party/bknr.datastore/src/skip-list/skip-list.lisp
@@ -21,7 +21,7 @@
   (declare (optimize speed))
   (do ((level 1 (1+ level)))
       ((or (= level +max-level+)
-	   (sl-random)) level)
+           (sl-random)) level)
     (declare (type fixnum level))))
 
 ;;; A node is a SIMPLE-VECTOR containing KEY, VALUE and the forward pointers
@@ -38,7 +38,7 @@
 (defun make-node (key value size &key initial-element)
   (let ((node (make-array (+ 2 size) :initial-element initial-element)))
     (setf (node-key node) key
-	  (node-value node) value)
+          (node-value node) value)
     node))
 
 (defun node-level (node)
@@ -50,10 +50,10 @@
 
 (defclass skip-list ()
   ((header :initform (make-header)
-	   :reader skip-list-header :type simple-vector)
+           :reader skip-list-header :type simple-vector)
    #+nil
    (finger :initform (make-header)
-	   :reader skip-list-finger :type simple-vector)
+           :reader skip-list-finger :type simple-vector)
    (length :initform 0 :accessor skip-list-length :type fixnum)
    (level :initform 0 :accessor skip-list-level :type fixnum))
   (:documentation "Skip-list class, access to elements is done through the header node."))
@@ -69,9 +69,9 @@
   "Follow a skip-list node at level LEVEL, stopping when there is no
 next node or when the next node has a larger key."
   (declare (type (or null simple-vector) node)
-	   (type integer key)
-	   (type integer level)
-	   (optimize (speed 3)))
+           (type integer key)
+           (type integer level)
+           (optimize (speed 3)))
   (do ((next (node-forward node level) (node-forward node level)))
       ((not (and next (< (node-key next) key))) node)
     (declare (type (or null simple-vector) next))
@@ -81,21 +81,21 @@ next node or when the next node has a larger key."
   "Search the list for the node before KEY, and construct an update
 array storing the previous nodes at all the levels."
   (declare (type integer key)
-	   (optimize (speed 3)))
+           (optimize (speed 3)))
   (let ((node (skip-list-header sl))
-	(update (make-header :initial-element (skip-list-header sl))))
+        (update (make-header :initial-element (skip-list-header sl))))
     (declare (type (or null simple-vector) node)
-	     (type simple-vector update)
-	     (optimize (speed 3)))
+             (type simple-vector update)
+             (optimize (speed 3)))
     
     (do ((level (1- (skip-list-level sl)) (1- level)))
-	((< level 0) (values update (node-forward node 0)))
+        ((< level 0) (values update (node-forward node 0)))
       (declare (type integer level))
 
       (setf (node-forward update level)
-	    (setf node (follow-node node key level))))))
+            (setf node (follow-node node key level))))))
 
-    
+
 (defmethod skip-list-insert ((sl skip-list) key value)
   "Insert VALUE under KEY in the skip-list. Replaces the existing node
 with KEY if present, or inserts a new node with random level."
@@ -106,30 +106,30 @@ with KEY if present, or inserts a new node with random level."
     (format t "update after first search: ~A~%" update)
     
     (if (and node (= (node-key node) key))
-	;; ein knoten mit dem KEY existiert schon, nur VALUE veraendern
-	(setf (node-value node) value)
+        ;; ein knoten mit dem KEY existiert schon, nur VALUE veraendern
+        (setf (node-value node) value)
 
-	;; es muss ein neuer knoten mit zufaelligem LEVEL eingefuegt werden
-	(let* ((new-level (random-level))
-	       (new-node (make-node key value new-level)))
+        ;; es muss ein neuer knoten mit zufaelligem LEVEL eingefuegt werden
+        (let* ((new-level (random-level))
+               (new-node (make-node key value new-level)))
 
-	  #+nil
-	  (format t "new-node ~A~%" new-node)
+          #+nil
+          (format t "new-node ~A~%" new-node)
 
-	  (when (> new-level (skip-list-level sl))
-	    (setf (skip-list-level sl) new-level))
-	  (incf (skip-list-length sl))
+          (when (> new-level (skip-list-level sl))
+            (setf (skip-list-level sl) new-level))
+          (incf (skip-list-length sl))
 
-	  ;; einfuegen des neuen knoten, indem der knoten in die
-	  ;; jeweiligen previous knoten an naechster stelle
-	  ;; eingetragen wird (dazu war UPDATE gut)
-	  (do ((level 0 (1+ level)))
-	      ((= level new-level) new-node)
-	    
-	    (let ((next (node-forward update level)))
-	      (when next
-		(setf (node-forward new-node level) (node-forward next level)
-		      (node-forward next level)      new-node))))))
+          ;; einfuegen des neuen knoten, indem der knoten in die
+          ;; jeweiligen previous knoten an naechster stelle
+          ;; eingetragen wird (dazu war UPDATE gut)
+          (do ((level 0 (1+ level)))
+              ((= level new-level) new-node)
+            
+            (let ((next (node-forward update level)))
+              (when next
+                (setf (node-forward new-node level) (node-forward next level)
+                      (node-forward next level)      new-node))))))
 
     value))
 
@@ -139,7 +139,7 @@ with KEY if present, or inserts a new node with random level."
   (do ((level (1- (skip-list-level sl)) (1- level))
        (header (skip-list-header sl)))
       ((or (< level 0)
-	   (node-forward header level))
+           (node-forward header level))
        (setf (skip-list-level sl) (1+ level))
        sl)))
 
@@ -150,10 +150,10 @@ with KEY if present, or inserts a new node with random level."
 
     (when (and node (= (node-key node) key))
       (do ((level 0 (1+ level)))
-	  ((= level (skip-list-level sl)))
-	(let ((next (node-forward update level)))
-	  (when (and next (eql (node-forward next level) node))
-	    (setf (node-forward next level) (node-forward node level)))))
+          ((= level (skip-list-level sl)))
+        (let ((next (node-forward update level)))
+          (when (and next (eql (node-forward next level) node))
+            (setf (node-forward next level) (node-forward node level)))))
 
       (decf (skip-list-length sl))
       (skip-list-reduce-header sl))
@@ -174,36 +174,36 @@ with KEY if present, or inserts a new node with random level."
 (defmethod skip-list-search-node ((sl skip-list) key)
   "Search for the node with KEY in the skip-list."
   (declare (type integer key)
-	       (optimize (speed 3)))
+           (optimize (speed 3)))
   (do ((level (1- (skip-list-level sl)) (1- level))
        (node (skip-list-header sl) (follow-node node key level)))
       ((< level 0)
        (let ((result (node-forward node)))
-	     (if (and result (= (node-key result) key))
-	         result
-	         nil)))
+         (if (and result (= (node-key result) key))
+             result
+             nil)))
     (declare (type fixnum level)
-	         (type simple-vector node))))
+             (type simple-vector node))))
 
 (defmethod skip-list-after-node ((sl skip-list) key)
   "Search for the node with key biffer or equal to KEY in the skip-list (for range queries)."
   (declare (type integer key)
-	       (optimize (speed 3)))
+           (optimize (speed 3)))
   (do ((level (1- (skip-list-level sl)) (1- level))
        (node (skip-list-header sl) (follow-node node key level)))
       ((< level 0)
        (let ((result (node-forward node)))
-	     (if (and result (>= (node-key result) key))
-	         result
-	         nil)))
+         (if (and result (>= (node-key result) key))
+             result
+             nil)))
     (declare (type fixnum level)
-	         (type simple-vector node))))
+             (type simple-vector node))))
 
 (defmethod skip-list-search ((sl skip-list) key &optional not-found)
   (let ((result (skip-list-search-node sl key)))
     (if result
-	    (node-value result)
-	    not-found)))
+        (node-value result)
+        not-found)))
 
 (defun node-before (node key &optional (level 0))
   (let ((next (node-forward node level)))
@@ -213,57 +213,57 @@ with KEY if present, or inserts a new node with random level."
 #+nil
 (defmethod skip-list-search ((sl skip-list) key &optional not-found)
   (let ((finger (skip-list-finger sl))
-	node
-	lvl)
+        node
+        lvl)
 
     (if (node-before finger key 0)
 
-	(do ((level 1 (1+ level)))
-	    ((or (>= level (skip-list-level sl))
-		 (not (node-before finger key level)))
-	     (setf node (node-forward finger (1- level))
-		   lvl (1- level))))
+        (do ((level 1 (1+ level)))
+            ((or (>= level (skip-list-level sl))
+                 (not (node-before finger key level)))
+             (setf node (node-forward finger (1- level))
+                   lvl (1- level))))
 
-	(do ((level 1 (1+ level)))
-	    ((or (>= level (skip-list-level sl))
-		 (node-before finger key level))
-	     
-	     (if (>= level (skip-list-level sl))
-		 (setf node (skip-list-header sl)
-		       lvl (1- (skip-list-level sl)))
-		 (setf node (node-forward finger level)
-		       lvl level)))
-	  #+nil(format t "level: ~A~%" level)))
+        (do ((level 1 (1+ level)))
+            ((or (>= level (skip-list-level sl))
+                 (node-before finger key level))
+             
+             (if (>= level (skip-list-level sl))
+                 (setf node (skip-list-header sl)
+                       lvl (1- (skip-list-level sl)))
+                 (setf node (node-forward finger level)
+                       lvl level)))
+          #+nil(format t "level: ~A~%" level)))
 
     #+nil(format t "node: ~A, level ~A~%" node lvl)
 
     (do ((level lvl (1- level)))
-	((or (null node)
-	     (< level 0))
-	 (when node (setf node (node-forward node 0))))
+        ((or (null node)
+             (< level 0))
+         (when node (setf node (node-forward node 0))))
 
       (setf node (follow-node node key level))
       (unless (eql node (skip-list-header sl))
-	#+nil(format t "storing in finger~%")
-	(setf (node-forward finger level) node)))
+        #+nil(format t "storing in finger~%")
+        (setf (node-forward finger level) node)))
 
     (if (and node (= (node-key node) key))
-	(node-value node)
-	not-found)))
+        (node-value node)
+        not-found)))
 
 (defmethod skip-list-to-list ((sl skip-list))
   (let ((node (skip-list-header sl)))
     (loop for next = (node-forward node) then (node-forward next)
-	  while next
-	  collect (list (node-key next) (node-value next)))))
+          while next
+          collect (list (node-key next) (node-value next)))))
 
 ;;; cursors
 
 (defclass skip-list-cursor ()
   ((node :initarg :node :accessor skip-list-cursor-node)
    (result :initform (list nil nil)
-                    :reader result
-                    :documentation "A cached list that will be used to avoid consing"))
+           :reader result
+           :documentation "A cached list that will be used to avoid consing"))
   (:documentation "Base cursor class for iterating through a skip-list. Maintains a reference
 to the current node and supports forward traversal via sl-cursor-next.
 
@@ -293,7 +293,7 @@ keys or values, use skip-list-keys-cursor or skip-list-values-cursor which retur
                 (second result) (bknr.skip-list::node-value node))
           (setf node (bknr.skip-list::node-forward node))
           result)
-	    eoc)))
+        eoc)))
 
 (defmethod sl-cursor-prev ((slc skip-list-cursor) &optional eoc)
   "Attempt to move the cursor backwards. Not supported for skip-lists - always signals an error.
@@ -311,8 +311,8 @@ Wraps the base skip-list-cursor to extract just the value portion from each node
 If at end of cursor, returns EOC unchanged."
   (let ((result (call-next-method)))
     (if (eql result eoc)
-	eoc
-	(second result))))
+        eoc
+        (second result))))
 
 (defclass skip-list-key-cursor (skip-list-cursor)
   ()
@@ -324,8 +324,8 @@ Wraps the base skip-list-cursor to extract just the key portion from each node."
 If at end of cursor, returns EOC unchanged."
   (let ((result (call-next-method)))
     (if (eql result eoc)
-	    eoc
-	    (first result))))
+        eoc
+        (first result))))
 
 (defmethod skip-list-cursor ((sl skip-list) &key cursor (class 'skip-list-cursor))
   "Create or reinitialize a cursor for skip-list SL.
@@ -334,8 +334,8 @@ If CURSOR is nil (default), creates a new cursor instance of CLASS (defaults to 
 The cursor starts positioned at the first element of the skip-list."
   (if cursor
       (progn (setf (skip-list-cursor-node cursor)
-		           (bknr.skip-list::node-forward (bknr.skip-list::skip-list-header sl)))
-	         cursor)
+                   (bknr.skip-list::node-forward (bknr.skip-list::skip-list-header sl)))
+             cursor)
       (make-instance class :node (bknr.skip-list::node-forward (bknr.skip-list::skip-list-header sl)))))
 
 (defmethod skip-list-values-cursor ((sl skip-list))
@@ -358,8 +358,8 @@ Stops iteration when encountering a node with key >= END."))
 Once a node with key >= END is encountered, returns EOC to signal end of range."
   (with-slots (node end) slc
     (if (and node (< (bknr.skip-list::node-key node) end))
-	    (call-next-method)
-	    eoc)))
+        (call-next-method)
+        eoc)))
 
 (defmethod skip-list-range-cursor ((sl skip-list) start end)
   "Create a cursor for iterating over keys in range [START, END) of skip-list SL.
@@ -374,7 +374,7 @@ positioned at the first node with key >= START, which will iterate until key >= 
 Uses a cursor to traverse the list from beginning to end."
   (let ((cursor (skip-list-cursor sl)))
     (do ((val (sl-cursor-next cursor) (sl-cursor-next cursor)))
-	    ((null val))
+        ((null val))
       (apply fun val))))
 
 (defmethod map-skip-list-values (fun (sl skip-list))
@@ -382,6 +382,6 @@ Uses a cursor to traverse the list from beginning to end."
 Uses a values-cursor to traverse only the values, ignoring keys."
   (let ((cursor (skip-list-values-cursor sl)))
     (do ((val (sl-cursor-next cursor) (sl-cursor-next cursor)))
-	    ((null val))
+        ((null val))
       (funcall fun val))))
 
