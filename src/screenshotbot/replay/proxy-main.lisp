41cbbafdbdc80b498fe405fe54b3d57e5d6ab442
Arnold Noronha <arnold@screenshotbot.io>
2025-11-27 23:20:01 -0500

    Setup Squid,... but...
    Summary: I'm not sure how the allowed_sites should look like yet.
    
    Differential Revision: https://phabricator.tdrhq.com/D11938
    
    GitOrigin-RevId: 68ef013c655f3673163f234e33480f611b1160b6

--- src/screenshotbot/replay/proxy-main.lisp
+++ src/screenshotbot/replay/proxy-main.lisp
@@ -16,16 +16,74 @@
    #:proxy-main/command))
 (in-package :screenshotbot/replay/proxy-main)
 
+(defun setup-squid ()
+  "Configure and start Squid proxy"
+  (log:info "Configuring Squid")
+
+  ;; Create allowed_sites file
+  (with-open-file (stream "/etc/squid/allowed_sites"
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+    (format stream "staging~%")
+    (format stream "replay~%")
+    (format stream "~%")
+    (format stream "# static IP for staging~%")
+    (format stream "172.29.1.15~%")
+    (format stream "~%")
+    (format stream "# second static IP for staging~%")
+    (format stream "172.20.0.6~%")
+    (format stream "~%")
+    (format stream "# static IP for replay~%")
+    (format stream "172.29.1.14~%")
+    (format stream "~%")
+    (format stream "# production over OpenVPN~%")
+    (format stream "10.9.8.1~%")
+    (format stream "~%")
+    (format stream "# debian-us-east-001~%")
+    (format stream "45.33.86.76~%"))
+
+  ;; Create squid.conf
+  (with-open-file (stream "/etc/squid/squid.conf"
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+    (format stream "http_port 3128~%")
+    (format stream "cache allow all~%")
+    (format stream "cache_dir aufs /var/spool/squid 50000 16 256~%")
+    (format stream "acl whitelist dstdomain \"/etc/squid/allowed_sites\"~%")
+    (format stream "http_access deny !whitelist~%"))
+
+  ;; Initialize cache directories
+  (log:info "Initializing Squid cache directories")
+  (uiop:run-program '("squid" "-z")
+                    :output :interactive
+                    :error-output :interactive
+                    :ignore-error-status t)
+
+  ;; Start Squid
+  (log:info "Starting Squid service")
+  (uiop:run-program '("systemctl" "start" "squid")
+                    :output :interactive
+                    :error-output :interactive)
+  (uiop:run-program '("systemctl" "enable" "squid")
+                    :output :interactive
+                    :error-output :interactive))
+
 (defun prepare-machine ()
-  "Prepare the machine by updating packages and installing Docker"
+  "Prepare the machine by updating packages and installing Docker and Squid"
   (log:info "Preparing machine: running apt-get update")
   (uiop:run-program '("apt-get" "update")
                     :output :interactive
                     :error-output :interactive)
-  (log:info "Installing Docker")
-  (uiop:run-program '("apt-get" "install" "-y" "docker.io")
+
+  (log:info "Installing Docker and Squid")
+  (uiop:run-program '("apt-get" "install" "-y" "docker.io" "squid")
                     :output :interactive
                     :error-output :interactive)
+
+  (setup-squid)
+
   (log:info "Machine preparation complete"))
 
 (defun proxy-main/handler (cmd)
