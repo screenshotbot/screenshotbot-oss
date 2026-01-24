3c6c9c06303390d0451de2b4d867ec1e59e96575
Arnold Noronha <arnold@screenshotbot.io>
2026-01-24 14:16:17 -0500

    Add CDN to Content-Security-Policy
    Summary: Fixes T2211
    
    Test Plan:
    Test via REPL in prod. Have not tested in Enterprise installs atm, there's a minor chance that the CDN domain name there can cause issues.
    
    Maniphest Tasks: T2211
    
    Differential Revision: https://phabricator.tdrhq.com/D12313
    
    GitOrigin-RevId: 2aa66c95ab837fdb1d09047395e2845d29ddd36d

--- src/screenshotbot/server.lisp
+++ src/screenshotbot/server.lisp
@@ -254,10 +254,10 @@ a no-op when either is nil."
         "csp-endpoint=\"/csp-report\"")
   (setf (hunchentoot:header-out "Content-Security-Policy")
         (format nil "~{~a~^; ~}"
-                '("default-src 'self'"
-                  "script-src 'self' 'unsafe-inline' https://js.stripe.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://unpkg.com https://assets.calendly.com https://code.jquery.com https://js.hs-scripts.com https://js.hs-banner.com https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://testimonial.to https://cdn.gdprlocal.com"
-                  "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com https://assets.calendly.com https://fonts.googleapis.com"
-                  "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net"
+                `("default-src 'self'"
+                  ,(format nil "script-src 'self' 'unsafe-inline' https://js.stripe.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://unpkg.com https://assets.calendly.com https://code.jquery.com https://js.hs-scripts.com https://js.hs-banner.com https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://testimonial.to https://cdn.gdprlocal.com ~a" util.cdn:*cdn-domain* )
+                  ,(format nil "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com https://assets.calendly.com https://fonts.googleapis.com ~a" util.cdn:*cdn-domain*)
+                  ,(format nil "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net ~a" util.cdn:*cdn-domain*)
                   "img-src 'self' data: https:"
                   "connect-src 'self' https: wss:"
                   "frame-src 'self' https://www.youtube.com https://assets.calendly.com https://embed-v2.testimonial.to https://www.googletagmanager.com https://app.hubspot.com"
