f96121bc620a24e65b2f71072bd31483b5b53f5c
Arnold Noronha <arnold@screenshotbot.io>
2026-02-16 14:19:51 -0500

    Fix another crash noise
    Differential Revision: https://phabricator.tdrhq.com/D12428
    
    GitOrigin-RevId: 9011c86646fd02b924225c39a9631a12266a2792

--- src/core/cli/test-sentry.lisp
+++ src/core/cli/test-sentry.lisp
@@ -27,6 +27,7 @@
       (+ 1 1)))
   (signals my-error
     (with-cli-sentry (:dry-run t
+                      :stream (make-string-output-stream)
                       :on-error (lambda ()))
       (error 'my-error))))
 
