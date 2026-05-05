a1fcde32549966909dd7b8d540be4041f5011eec
Arnold Noronha <arnold@screenshotbot.io>
2026-05-04 20:23:59 -0400

    New contact-us form
    Summary:
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D12867
    
    GitOrigin-RevId: 05bd47d4ca231f70897f35ae8329758fc3acdf62

--- src/screenshotbot/server.lisp
+++ src/screenshotbot/server.lisp
@@ -256,13 +256,13 @@ a no-op when either is nil."
   (setf (hunchentoot:header-out "Content-Security-Policy")
         (format nil "~{~a~^; ~}"
                 `("default-src 'self'"
-                  ,(format nil "script-src 'self' 'unsafe-inline' https://js.stripe.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://unpkg.com https://assets.calendly.com https://code.jquery.com https://js.hs-scripts.com https://js.hs-banner.com https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://testimonial.to https://cdn.gdprlocal.com https://*.hotjar.com https://*.cookiebot.com https://static.hsappstatic.net https://www.google.com https://www.gstatic.com https://plausible.io ~a" util.cdn:*cdn-domain* )
+                  ,(format nil "script-src 'self' 'unsafe-inline' https://js.stripe.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://unpkg.com https://assets.calendly.com https://code.jquery.com https://js.hs-scripts.com https://js.hs-banner.com https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://testimonial.to https://cdn.gdprlocal.com https://*.hotjar.com https://*.cookiebot.com https://static.hsappstatic.net https://www.google.com https://www.gstatic.com https://plausible.io https://js.hsforms.net ~a" util.cdn:*cdn-domain* )
                   ,(format nil "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com https://assets.calendly.com https://fonts.googleapis.com ~a" util.cdn:*cdn-domain*)
                   ,(format nil "font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net ~a" util.cdn:*cdn-domain*)
                   "img-src 'self' data: blob: https:"
                   ,(format nil "media-src 'self' ~a" util.cdn:*cdn-domain*)
                   "connect-src 'self' https: wss:"
-                  "frame-src 'self' https://www.youtube.com https://assets.calendly.com https://embed-v2.testimonial.to https://www.googletagmanager.com https://app.hubspot.com https://meetings.hubspot.com https://*.cookiebot.com"
+                  "frame-src 'self' https://www.youtube.com https://assets.calendly.com https://embed-v2.testimonial.to https://www.googletagmanager.com https://app.hubspot.com https://meetings.hubspot.com https://*.cookiebot.com https://js.hsforms.net"
                   "frame-ancestors 'none'"
                   "base-uri 'self'"
                   ;; Chrome enforces form-action on redirect targets,
