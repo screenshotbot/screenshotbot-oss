4b57d6146cd7e209cc16dbad927e5525211ea753
Arnold Noronha <arnold@screenshotbot.io>
2026-05-23 21:36:54 -0400

    The signin form is showing up now
    Differential Revision: https://phabricator.tdrhq.com/D12920
    
    GitOrigin-RevId: 1be226ea04c0689ae82c97b061e197e7aa935e67

--- src/screenshotbot/login/saml.lisp
+++ src/screenshotbot/login/saml.lisp
@@ -7,6 +7,7 @@
 (defpackage :screenshotbot/login/saml
   (:use #:cl)
   (:import-from #:core/installation/auth-provider
+                #:auth-provider-signin-form
                 #:auth-provider)
   (:import-from #:auth/login/roles-auth-provider
                 #:roles-auth-provider)
@@ -16,12 +17,30 @@
    #:saml-auth-provider))
 (in-package :screenshotbot/login/saml)
 
+(named-readtables:in-readtable markup:syntax)
+
 (defclass saml-auth-provider (auth-provider
                               roles-auth-provider)
   ((entity-id :initform "https://screenshotbot.io"
               :initarg :entity-id)
-   (idp-metadata-url :initarg :idp-metadata-url)))
+   (idp-metadata-url :initarg :idp-metadata-url)
+   (name :initarg :name
+         :initform "generic-saml"
+         :reader saml-name)))
 
 (defhandler (nil :uri "/sso/saml/callback") ()
   (error "unimpl"))
 
+(defmethod signin-link ((self saml-auth-provider) redirect)
+  "#")
+
+(defmethod logo-svg ((self saml-auth-provider))
+  nil)
+
+(defmethod auth-provider-signin-form ((self saml-auth-provider) redirect)
+  <div class= "form-group mt-1 text-center mb-0">
+    <a class= "btn btn-outline-secondary" style= "width:100%"  href= (signin-link self redirect) >
+      ,(logo-svg self)
+      <span class= "ms-1">Sign in with ,(saml-name self) </span>
+    </a>
+  </div>)
