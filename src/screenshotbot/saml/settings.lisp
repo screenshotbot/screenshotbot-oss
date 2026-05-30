ddef44f797cb12ec833f2f8b6f6d3096d90e98c9
Arnold Noronha <arnold@screenshotbot.io>
2026-05-30 15:04:25 -0400

    Create a form for creating a SAML connection
    Differential Revision: https://phabricator.tdrhq.com/D12954
    
    GitOrigin-RevId: ed1e7ca10c7078ff1bc4044a1e839b4924de2cd6

--- src/screenshotbot/saml/settings.lisp
+++ src/screenshotbot/saml/settings.lisp
@@ -11,12 +11,17 @@
   (:import-from #:screenshotbot/template
                 #:app-template)
   (:import-from #:core/ui/taskie
+                #:taskie-page-title
                 #:taskie-list)
   (:import-from #:screenshotbot/login/saml
                 #:saml-entity-id
                 #:saml-auth-providers-for-company)
   (:import-from #:screenshotbot/login/common
-                #:with-login))
+                #:with-login)
+  (:import-from #:nibble
+                #:nibble)
+  (:import-from #:core/ui/simple-card-page
+                #:simple-card-page))
 (in-package :screenshotbot/saml/settings)
 
 (named-readtables:in-readtable markup:syntax)
@@ -29,11 +34,11 @@
   (check-enabled!)
   (with-login ()
     <app-template>
-      <div class= "page-title-box">
-        <h4>Self service SSO configs</h4>
-      </div>
+      <taskie-page-title title= "Configure Single-Sign-on">
+        <a href= (nibble () (%new-sso)) class= "btn btn-primary mb-2" >Create</a>
+      </taskie-page-title>
 
-      <table class= "table border">
+      <table class= "table border mt-3">
         <thead>
           <th>Type</th>
           <th>IdP Entity ID</th>
@@ -51,5 +56,42 @@
                 </tr>)
       </table>
     </app-template>))
+
+(defun sso-form ()
+  <simple-card-page>
+    <div class= "card-header">
+    <h4 class= "card-title">
+      New SAML connection
+    </h4>
+    </div>
+
+    <div class= "mb-2" >
+      <label for= "name" class= "form-label">Friendly Name</label>
+      <div class= "text-muted">
+        This will only be used to distinguish between different authentication methods
+      </div>
+      <input type= "text" id= "name" name= "name" class= "form-control"  />
+    </div>
+    
+    <div class= "mb-2" >
+      <label for= "sp-metadata" class= "form-label">Service Provider EntityID / Metadata URL</label>
+      <div class= "text-muted">
+        Copy this into your Identity Provider
+      </div>      
+      <input type= "text" id= "sp-metadata" disabled= "disabled" class= "form-control"  value= (hex:make-full-url hunchentoot:*request* "/saml/metadata") />
+    </div>
+
+    <div class= "mb-2" >
+      <label for= "idp-metadata" class= "form-label">Identity Provider Metadata URL</label>
+      <input type= "text" name= "idp-metadata" id= "idp-metadata" class= "form-control"   placeholder= "https://idp.example.com/saml/metadata" />
+    </div>
+    
+    <div class= "card-footer">
+      <input type= "submit" class= "btn btn-primary" value= "Create" />
+    </div>
+  </simple-card-page>)
+
+(defun %new-sso ()
+  (sso-form))
 
 
