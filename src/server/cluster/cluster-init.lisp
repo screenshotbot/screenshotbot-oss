1d2a0685af1c2ee8341e0f34e7dedfa47797650d
Arnold Noronha <arnold@screenshotbot.io>
2026-07-09 09:46:03 -0400

    Grow the cluster using a more careful script
    Summary:
    ...
    
    ...
    
    ...
    
    Test Plan:
    run-cloudformation.sh succeeded
    
    Differential Revision: https://phabricator.tdrhq.com/D13058
    
    GitOrigin-RevId: c76b1863fc2ff0acac2a8ac845abb5db2089676b

--- src/server/cluster/cluster-init.lisp
+++ src/server/cluster/cluster-init.lisp
@@ -9,7 +9,11 @@
   (:import-from #:util/store/store
                 #:ec2-get-local-ipv4
                 #:ec2-store
-                #:make-default-store))
+                #:make-default-store)
+  (:import-from #:util/request
+                #:http-request)
+  (:import-from #:alexandria
+                #:assoc-value))
 (in-package :server/cluster/cluster-init)
 
 (defun cluster-init/command ()
@@ -27,30 +31,67 @@
               :string
               :key :other-peers
               :long-name "other-peers"
-              :description "The other peers (excluding this one). Command separated IP addresses without port numbers."))))
+              :description "Deprecated. Keep blank."))))
 
 (defun cluster-init/handler (cmd)
- (let ((store (str:ensure-suffix "/" (clingon:getopt cmd :store))))
-   (init-raft-config store
-                     (remove-if #'str:emptyp
+  (log:config :debug)
+  (let ((store (str:ensure-suffix "/" (clingon:getopt cmd :store))))
+    (init-raft-config store
+                      (remove-if #'str:emptyp
                                 (str:split "," (or (clingon:getopt cmd :other-peers)
                                                    ""))))))
 
+(defun id-to-ip (conf)
+  (first (str:rsplit ":" conf :limit 4)))
 
 (defun init-raft-config (store other-peers)
-  (let ((raft-config (path:catfile store "raft-config.lisp")))
-    (uiop:with-staging-pathname (raft-config raft-config)
-      (with-open-file (stream raft-config :direction :output :if-exists :append)
+  (let ((raft-config (path:catfile store "raft-config.lisp"))
+        (my-ip (ec2-get-local-ipv4)))
+    (cond
+      ((path:-e raft-config)
+       ;; We're already dealing with an existing cluster
+       (log:info "Growing existing cluster")
+       (grow-cluster my-ip
+                     (id-to-ip (str:trim (uiop:read-file-string (path:catfile store "leader.txt"))))))
+      (t
+       (make-dirs)
+       (uiop:with-staging-pathname (raft-config raft-config)
+         (with-open-file (stream raft-config :direction :output :if-exists :append)
         (let ((*package* (find-package :cl-user)))
           (format stream "~s"
                   `(make-default-store
                     'ec2-store
                     :group "screenshotbot"
-                    :data-path ,(namestring "~/raft-data/")
+                    :data-path ,(namestring "~arnold/raft-data/")
                     :port 7070
                     :ips (list
-                          ,(ec2-get-local-ipv4)
+                          ,my-ip
                           ,@(loop for peer in other-peers
                                   collect (format nil "~a" peer))))))))
-    (log:info "Updated ~a" raft-config)))
+       (log:info "Updated ~a" raft-config)))))
+
+(defun %shell (cmd)
+  (uiop:run-program
+   cmd
+   :output t
+   :error-output t))
+
+(defun make-dirs ()
+  (%shell "sudo mkdir /mnt/efs/fs1/screenshotbot -p")
+  (%shell "sudo chown arnold:arnold /mnt/efs/fs1/screenshotbot")
+  (%shell "sudo -u arnold mkdir -p /mnt/efs/fs1/screenshotbot/object-store")
+  (%shell "sudo -u arnold mkdir -p /mnt/efs/fs1/screenshotbot/logs"))
+
+(defun grow-cluster (my-ip leader)
+  ;; First, get the current cluster config:
+  (log:info "Waiting 30s to make sure the screenshotbot service has started")
+  (sleep 30)
+  (let* ((config (json:decode-json-from-string
+                  (http-request (format nil "http://~a:4001/raft-state?full=true" leader))))
+         (peers (assoc-value config :peers)))
+    (log:info "Running braft_cli add_peer")
+    (%shell (format nil
+                    "braft_cli add_peer --group=screenshotbot --conf=~a --peer=~a "
+                    (str:join "," peers)
+                    (format nil "~a:7070:0" my-ip)))))
 
