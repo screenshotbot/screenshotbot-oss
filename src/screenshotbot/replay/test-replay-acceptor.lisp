96a075c3cdc6bb9631d73eb6fb66ba267ac41c7e
Arnold Noronha <arnold@screenshotbot.io>
2026-09-21 15:52:42 -0400

    Revert "route the replay snapshots via the standard acceptor too"
    This reverts commit 2ae4cf5a79161fcb7740930d1e30123177d50b9d.
    
    GitOrigin-RevId: dfac7a05fec214cf084e41c4808cf6d5b9cce77a

--- src/screenshotbot/replay/test-replay-acceptor.lisp
+++ src/screenshotbot/replay/test-replay-acceptor.lisp
@@ -38,8 +38,6 @@
                 #:with-global-binding)
   (:import-from #:flexi-streams
                 #:make-in-memory-output-stream)
-  (:import-from #:core/config/api
-                #:config)
   (:local-nicknames (#:a #:alexandria)))
 (in-package :screenshotbot/replay/test-replay-acceptor)
 
@@ -53,8 +51,7 @@
                     (status 200))
   (with-test-store (:globally t)
     (tmpdir:with-tmpdir (tmpdir)
-      (setf (config "replay.render-acceptor.domain") "127.0.0.1")
-      (with-local-acceptor (host :acceptor acceptor)
+     (with-local-acceptor (host :acceptor acceptor)
          ('render-acceptor)
        (unless (eql status 404)
          (fad:copy-file *fixture* (path:catfile tmpdir "abcd00.png")))
